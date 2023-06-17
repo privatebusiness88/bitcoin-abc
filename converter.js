4ad78f0924d12a1011a41773338e36f617652861
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-18 00:26:15 +0700

    Create converter.js

---
+++ converter.js
@@ -0,0 +1,31 @@
+package '#main'
+let converter = new Vue({
+    el: '#converter',
+    data : {
+        denomination : "",
+        figure : "",
+        period :"",
+    },
+    methods: {
+        periodic(event){
+            this.period = event.target.value
+            console.log(this.period)
+            
+        },
+
+        converter_func(){
+            if (this.period == 'NEW' ){
+                this.denomination = this.figure / 1000
+    
+            }else if(this.period == 'OLD'){
+               this.denomination = this.figure * 1000
+            } else {
+               this.denomination = this.figure /1000
+    
+            }
+            console.log(this.denomination)
+
+        }
+
+    }
+})
