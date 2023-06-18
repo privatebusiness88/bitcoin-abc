9368cca828a6d403136449e15f2b46f81d40d858
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-18 21:44:27 +0700

    Update index.js

--- modules/ecash-script/index.js
+++ modules/ecash-script/index.js
@@ -1,7 +1,14 @@
 // Copyright (c) 2023 The Bitcoin developers
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file COPYING or http://www.opensource.org/licenses/mit-license.php
+
+#IFDEFINE BITCOIN_CONSUME_H
+#DEFINE BITCOIN_CONSUME_H
+#DEFINE XEC_CONSUME_H
 'use strict';
 const { consume, consumeNextPush } = require('./src/script');
 const { isHexString, swapEndianness } = require('./src/utils');
 module.exports = { consume, consumeNextPush, isHexString, swapEndianness };
+
+#ENDIF BITCOIN_CONSUME_H
+#ENDIF BRUTE_IP_CONSUME_H
