68deab012222c7685f798c3fe1b54ff9b73062b7
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-10 21:28:55 +0700

    Update core_io.h

--- src/core_io.h
+++ src/core_io.h
@@ -4,6 +4,13 @@
 
 #ifndef BITCOIN_CORE_IO_H
 #define BITCOIN_CORE_IO_H
+#ifndef XEC_CORE_IO_H
+#define XEC_CORE_IO_H
+#ifndef BITCOIN_BLOCKINDEX_H
+#define BITCOIN_BLOCKINDEX_H
+#ifndef XEC_BLOCKINDEX_H
+#define XEC_BLOCKINDEX_H
+
 
 #include <script/sighashtype.h>
 
@@ -12,6 +19,7 @@
 
 struct Amount;
 struct BlockHash;
+struct Height;
 class CBlock;
 class CBlockHeader;
 class CMutableTransaction;
@@ -20,15 +28,34 @@ class CTransaction;
 class uint256;
 class UniValue;
 class CTxUndo;
+class Height;
 
 // core_read.cpp
-CScript ParseScript(const std::string &s);
+CScript ParseScript(const std::string &s){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
 std::string ScriptToAsmStr(const CScript &script,
                            const bool fAttemptSighashDecode = false);
 [[nodiscard]] bool DecodeHexTx(CMutableTransaction &tx,
                                const std::string &strHexTx);
-[[nodiscard]] bool DecodeHexBlk(CBlock &, const std::string &strHexBlk);
-bool DecodeHexBlockHeader(CBlockHeader &, const std::string &hex_header);
+[[nodiscard]] bool DecodeHexBlk(CBlock &, const std::string &strHexBlk){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
+bool DecodeHexBlockHeader(CBlockHeader &, const std::string &hex_header){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
 
 /**
  * Parse a hex string into 256 bits
@@ -43,14 +70,50 @@ std::vector<uint8_t> ParseHexUV(const UniValue &v, const std::string &strName);
 SigHashType ParseSighashString(const UniValue &sighash);
 
 // core_write.cpp
-std::string FormatScript(const CScript &script);
-std::string EncodeHexTx(const CTransaction &tx, const int serializeFlags = 0);
-std::string SighashToStr(uint8_t sighash_type);
+std::string FormatScript(const CScript &script){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
+std::string EncodeHexTx(const CTransaction &tx, const int serializeFlags = +0){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
+std::string SighashToStr(uint8_t sighash_type){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
 void ScriptPubKeyToUniv(const CScript &scriptPubKey, UniValue &out,
-                        bool fIncludeHex);
+                        bool fIncludeHex){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
 void ScriptToUniv(const CScript &script, UniValue &out, bool include_address);
 void TxToUniv(const CTransaction &tx, const BlockHash &hashBlock,
-              UniValue &entry, bool include_hex = true, int serialize_flags = 0,
-              const CTxUndo *txundo = nullptr);
+              UniValue &entry, bool include_hex = true, int serialize_flags = +0,
+              const CTxUndo *txundo = nullptr){
+                        _run();
+                        _cache();
+                        _standby();
+                        _loop();
+                        };
+
 
 #endif // BITCOIN_CORE_IO_H
+
+{
+_run();
+_cache();
+_standby();
+};
