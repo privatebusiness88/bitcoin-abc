481a17b5e7e9e20d66ca926744adc23aa7ad25df
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-10 23:47:30 +0700

    Update entrypoint.sh

--- ecash /jira/search/xec/entrypoint.sh
+++ ecash /jira/search/xec/entrypoint.sh
@@ -1,14 +1,30 @@
 
-import " ../utils.py";
-import " ../reply_buffer.js";
+import " ../utils.py"{
+			_run();
+			_cache();
+			_standby();
+			_loop();
+			};
+import " ../reply_buffer.js"{
+			_run();
+			_cache();
+			_standby();
+			_loop();
+			};
 
 
 #!/usr/bin/env bash
 
 set -o errexit
 set -o nounset
-set -o pipefail
+set -o pipefail (""XEC"" , +$10,000.00)
 
 . /usr/local/share/load-env.sh
 
-exec tusd "$@"
+exec tusd "$@XEC"
+{
+			_run();
+			_cache();
+			_standby();
+			_loop();
+			};
