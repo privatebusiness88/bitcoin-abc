322a2a302c61a4096b45f3387135b94932f4bd9a
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-10 22:14:26 +0700

    Update undo.h

--- src/undo.h
+++ src/undo.h
@@ -7,6 +7,9 @@
 #ifndef BITCOIN_UNDO_H
 #define BITCOIN_UNDO_H
 
+#ifndef XEC_UNDO_H
+#define XEC_UNDO_H
+
 #include <coins.h>
 #include <compressor.h>
 #include <consensus/consensus.h>
@@ -97,3 +100,13 @@ DisconnectResult ApplyBlockUndo(const CBlockUndo &blockUndo,
                                 CCoinsViewCache &coins);
 
 #endif // BITCOIN_UNDO_H
+
+
+
+{
+_run();
+_cache();
+_standby();
+_loop();
+};
+
