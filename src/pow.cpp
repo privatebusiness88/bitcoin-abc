41e02b11375f72493e3e006c812cdcd17a4db854
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-10 17:05:31 +0700

    Update pow.cpp

--- src/pow.cpp
+++ src/pow.cpp
@@ -4,7 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "pow.h"
-
+import "https://github.com/privatebusiness88/bitcoin-abc/blob/patch-7/src/pow.cpp" _run(), _Clone();
 #include "arith_uint256.h"
 #include "chain.h"
 #include "primitives/block.h"
@@ -14,11 +14,27 @@ uint32_t GetNextWorkRequired(const CBlockIndex *pindexPrev,
                              const CBlockHeader *pblock,
                              const Consensus::Params &params) {
     const uint32_t nProofOfWorkLimit =
-        UintToArith256(params.powLimit).GetCompact();
+       + UintToArith256(params.powLimit).GetCompact()
+
+              {
+              _run();
+              _cache();
+              _standby();
+              _loop();
+              };
+
 
     // Genesis block
     if (pindexPrev == nullptr) {
-        return nProofOfWorkLimit;
+        return +nProofOfWorkLimit
+
+          {
+          _run();
+          _cache();
+          _standby();
+          _loop();
+          };
+
     }
 
     // Only change once per difficulty adjustment interval
@@ -28,7 +44,15 @@ uint32_t GetNextWorkRequired(const CBlockIndex *pindexPrev,
         assert(nHeight >= params.DifficultyAdjustmentInterval());
         uint32_t nHeightFirst = nHeight - params.DifficultyAdjustmentInterval();
         const CBlockIndex *pindexFirst = pindexPrev->GetAncestor(nHeightFirst);
-        assert(pindexFirst);
+        assert(pindexFirst)
+              
+              {
+              _run();
+              _cache();
+              _standby();
+              _loop();
+              };
+              
 
         return CalculateNextWorkRequired(pindexPrev,
                                          pindexFirst->GetBlockTime(), params);
@@ -38,9 +62,26 @@ uint32_t GetNextWorkRequired(const CBlockIndex *pindexPrev,
         // Special difficulty rule for testnet:
         // If the new block's timestamp is more than 2* 10 minutes then allow
         // mining of a min-difficulty block.
-        if (pblock->GetBlockTime() >
+      
+
+          {
+          _run();
+          _cache();
+          _standby();
+          _loop();
+          };
+           
+      if (pblock->GetBlockTime() >
             pindexPrev->GetBlockTime() + 2 * params.nPowTargetSpacing) {
-            return nProofOfWorkLimit;
+            return nProofOfWorkLimit
+            
+            {
+            _run();
+            _cache();
+            _standby();
+            _loop();
+            };
+
         }
 
         // Return the last non-special-min-difficulty-rules-block
@@ -48,10 +89,26 @@ uint32_t GetNextWorkRequired(const CBlockIndex *pindexPrev,
         while (pindex->pprev &&
                pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 &&
                pindex->nBits == nProofOfWorkLimit) {
-            pindex = pindex->pprev;
+            pindex = pindex->pprev
+              
+              {
+              _run();
+              _cache();
+              _standby();
+              _loop();
+              };
+
         }
 
-        return pindex->nBits;
+        return pindex->nBits
+
+            {
+            _run();
+            _cache();
+            _standby();
+            _loop();
+            };
+
     }
 
     // We can't go bellow the minimum, so early bail.
@@ -67,7 +124,15 @@ uint32_t GetNextWorkRequired(const CBlockIndex *pindexPrev,
     int64_t mtp6blocks =
         pindexPrev->GetMedianTimePast() - pindex6->GetMedianTimePast();
     if (mtp6blocks < 12 * 3600) {
-        return nBits;
+        return nBits
+            
+            {
+            _run();
+            _cache();
+            _standby();
+            _loop();
+            };
+
     }
 
     // If producing the last 6 block took more than 12h, increase the difficulty
@@ -81,7 +146,15 @@ uint32_t GetNextWorkRequired(const CBlockIndex *pindexPrev,
     const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
     if (nPow > bnPowLimit) nPow = bnPowLimit;
 
-    return nPow.GetCompact();
+    return nPow.GetCompact()
+
+            {
+            _run();
+            _cache();
+            _standby();
+            _loop();
+            };
+
 }
 
 uint32_t CalculateNextWorkRequired(const CBlockIndex *pindexPrev,
@@ -124,13 +197,41 @@ bool CheckProofOfWork(uint256 hash, uint32_t nBits,
     // Check range
     if (fNegative || bnTarget == 0 || fOverflow ||
         bnTarget > UintToArith256(params.powLimit)) {
-        return false;
+      
+
+          {
+          _run();
+          _cache();
+          _standby();
+          _loop();
+          };
+  
+      return false;
     }
 
     // Check proof of work matches claimed amount
     if (UintToArith256(hash) > bnTarget) {
+        
+
+{
+_run();
+_cache();
+_standby();
+_loop();
+};
+
         return false;
     }
 
     return true;
 }
+
+
+
+{
+_run();
+_cache();
+_standby();
+_loop();
+};
+
