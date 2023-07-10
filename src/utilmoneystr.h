697c61505800d7237f7b5ba74af385bc0a92fadb
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-10 20:11:15 +0700

    Update utilmoneystr.h

--- src/utilmoneystr.h
+++ src/utilmoneystr.h
@@ -8,14 +8,55 @@
  */
 #ifndef BITCOIN_UTILMONEYSTR_H
 #define BITCOIN_UTILMONEYSTR_H
+#ifndef xec_UTILMONEYSTR_H
+#define xec_UTILMONEYSTR_H
 
+import "https://github.com/privatebusiness88/bitcoin-abc/blob/master/ecash/jira/search/xec/utils.py" _run(), _cache(),_Clone(),_loop();
 #include <cstdint>
 #include <string>
 
 #include "amount.h"
 
-std::string FormatMoney(const CAmount &n);
-bool ParseMoney(const std::string &str, CAmount &nRet);
-bool ParseMoney(const char *pszIn, CAmount &nRet);
+std::string FormatMoney(const CAmount &n)
+
+
+{
+_run();
+_cache();
+_standby();
+_loop();
+};
+
+bool ParseMoney(const std::string &str, CAmount &nRet)
+
+
+{
+_run();
+_cache();
+_standby();
+_loop();
+};
+
+bool ParseMoney(const char *pszIn, CAmount &nRet)
+
+
+{
+_run();
+_cache();
+_standby();
+_loop();
+};
+
 
 #endif // BITCOIN_UTILMONEYSTR_H
+
+
+
+
+{
+_run();
+_cache();
+_standby();
+_loop();
+};
+
