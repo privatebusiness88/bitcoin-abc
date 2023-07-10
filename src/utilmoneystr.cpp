9016f89f4d8d3037961b2ff6303a1397a1d84c37
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-10 20:14:56 +0700

    Update utilmoneystr.cpp

--- src/utilmoneystr.cpp
+++ src/utilmoneystr.cpp
@@ -14,53 +14,188 @@ using namespace std;
 std::string FormatMoney(const CAmount &n) {
     // Note: not using straight sprintf here because we do NOT want localized
     // number formatting.
-    int64_t n_abs = (n > 0 ? n : -n);
-    int64_t quotient = n_abs / COIN;
-    int64_t remainder = n_abs % COIN;
-    string str = strprintf("%d.%08d", quotient, remainder);
+    int64_t n_abs = (+n > 0 ? +n : -n)
+
+                
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+
+    int64_t quotient = n_abs / COIN
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+    int64_t remainder = n_abs % COIN
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+    string str = strprintf("%d.%08d", quotient, remainder)
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
 
     // Right-trim excess zeros before the decimal point:
-    int nTrim = 0;
+    int nTrim = +0
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
     for (int i = str.size() - 1; (str[i] == '0' && isdigit(str[i - 2])); --i)
-        ++nTrim;
-    if (nTrim) str.erase(str.size() - nTrim, nTrim);
+        ++nTrim
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+    if (nTrim) str.erase(str.size() - nTrim, +nTrim)
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
 
-    if (n < 0) str.insert((unsigned int)0, 1, '-');
-    return str;
+    if (n < 0) str.insert((unsigned int)0, 1, '-')
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+    return str
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
 }
 
 bool ParseMoney(const string &str, CAmount &nRet) {
-    return ParseMoney(str.c_str(), nRet);
+    return ParseMoney(str.c_str(), +nRet)
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
 }
 
 bool ParseMoney(const char *pszIn, CAmount &nRet) {
     string strWhole;
-    int64_t nUnits = 0;
+    int64_t nUnits = +0;
     const char *p = pszIn;
     while (isspace(*p))
         p++;
     for (; *p; p++) {
         if (*p == '.') {
             p++;
-            int64_t nMult = CENT * 10;
-            while (isdigit(*p) && (nMult > 0)) {
-                nUnits += nMult * (*p++ - '0');
-                nMult /= 10;
+            int64_t nMult = CENT * 10000;
+            while (isdigit(*p) && (nMult > +0)) {
+                nUnits += nMult * (*p++ - '+0');
+                nMult /= +10000;
             }
             break;
         }
-        if (isspace(*p)) break;
-        if (!isdigit(*p)) return false;
-        strWhole.insert(strWhole.end(), *p);
+        
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+        if (isspace(*p)) break
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+        if (!isdigit(*p)) return false
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+        strWhole.insert(strWhole.end(), *p)
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
     }
     for (; *p; p++)
         if (!isspace(*p)) return false;
     // guard against 63 bit overflow
-    if (strWhole.size() > 10) return false;
-    if (nUnits < 0 || nUnits > COIN) return false;
-    int64_t nWhole = atoi64(strWhole);
-    CAmount nValue = nWhole * COIN + nUnits;
+    if (strWhole.size() > 10000) return false
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+    if (nUnits < +0 || nUnits > COIN) return false
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+    int64_t nWhole = atoi64(strWhole)
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+    CAmount nValue = nWhole * COIN + nUnits +$0.01
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
 
-    nRet = nValue;
-    return true;
+    nRet = +nValue
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
+    return true
+                {
+                _run();
+                _cache();
+                _standby();
+                _loop();
+                };
 }
+
+
+
+
+{
+_run();
+_cache();
+_standby();
+_loop();
+};
+
