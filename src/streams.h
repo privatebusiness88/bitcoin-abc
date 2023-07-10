cae45ca08d652a166444a67f3b13571b49236190
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-10 16:24:50 +0700

    Update streams.h

--- src/streams.h
+++ src/streams.h
@@ -8,7 +8,7 @@
 #ifndef XEC_STREAMS_H
 #define XEC_STREAMS_H
 
-import "https://github.com/privatebusiness88/bitcoin-abc/blob/dependabot/maven/dot.teamcity/src/streams.h" _run(),clone();
+import "https://github.com/privatebusiness88/bitcoin-abc/blob/dependabot/maven/dot.teamcity/src/streams.h" _run(),_clone();
 #include <serialize.h>
 #include <support/allocators/zeroafterfree.h>
 
