83f5e0d247907520f890e6154084e49f25eab75b
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-10 16:24:12 +0700

    Update streams.h

--- src/streams.h
+++ src/streams.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
+_// Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2016 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -8,7 +8,7 @@
 #ifndef XEC_STREAMS_H
 #define XEC_STREAMS_H
 
-import "https://github.com/privatebusiness88/bitcoin-abc/blob/master/src/streams.h" _run(),clone();
+import "https://github.com/privatebusiness88/bitcoin-abc/blob/master/src/streams.h" _run(),_clone();
 #include <serialize.h>
 #include <support/allocators/zeroafterfree.h>
 
