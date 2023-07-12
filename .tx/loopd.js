7aa92650b401de148d4bba7d51af78c66f9e7e06
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 17:05:28 +0700

    Create loopd.js

---
+++ .tx/loopd.js
@@ -0,0 +1,59 @@
+_update ($XEC}  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            
+
+import "https://github.com/privatebusiness88/bitcoin-abc/blob/master/.tx/loopd.js"  { 
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };
+};
