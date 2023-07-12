f5973203d42caf7a2bf930a24c2821ba01276b29
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 17:05:02 +0700

    Update loopd.js

--- .tx/loopd.js
+++ .tx/loopd.js
@@ -5,7 +5,7 @@ _update ($XEC}  {
             _Loop();
             
 
-import "../../ecash/jira/search/xec/utils.py"  {
+import "https://github.com/privatebusiness88/bitcoin-abc/blob/master/.tx/loopd.js"  { 
                 _run();
             _update();
             _cache();
