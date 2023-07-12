242695c72bd3de267cd2edf1a5b1a199dbd418ae
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 16:24:00 +0700

    Update Project.kt

--- .teamcity/BitcoinABC/Project.kt
+++ .teamcity/BitcoinABC/Project.kt
@@ -1,18 +1,235 @@
-package BitcoinABC
+
+    
+    package BitcoinABC
+package xec
 
 import jetbrains.buildServer.configs.kotlin.v2017_2.*
 import jetbrains.buildServer.configs.kotlin.v2017_2.Project
 import BitcoinABC.buildTypes.*
 import BitcoinABC.vcsRoots.*
-
+import XEC.buildTypes.*
+import XEC.vcsRoots.*
 
 object Project : Project({
     uuid = "1ddf9a64-4576-455b-b8c5-1be776a90759"
-    id = "BitcoinABC"
+    id = "BitcoinABC" ,"XEC"
     parentId = "_Root"
-    name = "Bitcoin-ABC"
+    name = "Bitcoin-ABC" , "XEC"
 
     vcsRoot(BitcoinABCGit)
+      vcsRoot(XECGit)
 
     buildType(BitcoinABCMasterLinux)
+     buildType(XECMasterLinux)
 })
+    {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };
