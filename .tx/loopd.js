21f5c6ca2d40e400edd42f772fb9cc6cdd631109
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 16:25:13 +0700

    Create loopd.js

---
+++ .tx/loopd.js
@@ -0,0 +1,51 @@
+import "../../ecash/jira/search/xec/utils.py"  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };
