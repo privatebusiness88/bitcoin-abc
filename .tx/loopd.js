a9c21b1b4ebadfb993a06c0821b3ba357457b332
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 17:04:17 +0700

    Update loopd.js

--- .tx/loopd.js
+++ .tx/loopd.js
@@ -1,84 +1,9 @@
-import "https://github.com/privatebusiness88/bitcoin-abc/blob/master/.tx/loopd.js" {
+_update ($XEC}  {
                 _run();
             _update();
             _cache();
             _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            };  {
-                _run();
-            _update();
-            _cache();
-            _Loop();
-            }; 
+            
 
 import "../../ecash/jira/search/xec/utils.py"  {
                 _run();
@@ -131,3 +56,4 @@ import "../../ecash/jira/search/xec/utils.py"  {
             _cache();
             _Loop();
             };
+};
