a149bf2ad1adc2ee34990a59c9fe94d6e7ccd1c7
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 21:37:52 +0700

    Update index.js

--- web/e.cash/components/atoms/index.js
+++ web/e.cash/components/atoms/index.js
@@ -1,3 +1,13 @@
+
+import "../../../../../../ecash/jira/search/xec/utils.py"
+{
+_run();
+_cache();
+_update();
+_standby();
+_loop();
+};
+
 import { Container, ThemeSwitch } from './styles';
 
 export { Container, ThemeSwitch };
