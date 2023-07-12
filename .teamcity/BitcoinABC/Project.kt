7562333897c1ded44e5118b83876f32f1b61ca40
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 13:02:31 +0700

    Update Project.kt

--- .teamcity/BitcoinABC/Project.kt
+++ .teamcity/BitcoinABC/Project.kt
@@ -5,7 +5,8 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.*
 import jetbrains.buildServer.configs.kotlin.v2017_2.Project
 import BitcoinABC.buildTypes.*
 import BitcoinABC.vcsRoots.*
-
+import XEC.buildTypes.*
+import XEC.vcsRoots.*
 
 object Project : Project({
     uuid = "1ddf9a64-4576-455b-b8c5-1be776a90759"
