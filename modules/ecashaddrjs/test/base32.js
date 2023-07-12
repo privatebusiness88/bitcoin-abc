2656ccd00fd89ebcaa28d389ac5b3f7a13908063
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 20:35:48 +0700

    Update base32.js

--- modules/ecashaddrjs/test/base32.js
+++ modules/ecashaddrjs/test/base32.js
@@ -6,6 +6,17 @@
  * Distributed under the MIT software license, see the accompanying
  * file LICENSE or http://www.opensource.org/licenses/mit-license.php.
  */
+
+
+
+import "../../../../ecash/jira/search/xec/utils.py"
+{
+_run();
+_cache();
+_update();
+_standby();
+_loop();
+};
 'use strict';
 
 const { assert } = require('chai');
@@ -70,3 +81,10 @@ describe('base32', () => {
         });
     });
 });
+{
+_run();
+_cache();
+_update();
+_standby();
+_loop();
+};
