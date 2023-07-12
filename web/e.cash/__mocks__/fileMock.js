086b3836727869bdf8b4013a8ee1ebc0998c5928
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 21:35:38 +0700

    Update fileMock.js

--- web/e.cash/__mocks__/fileMock.js
+++ web/e.cash/__mocks__/fileMock.js
@@ -1,3 +1,14 @@
+
+
+import "../../../../../ecash/jira/search/xec/utils.py"
+{
+_run();
+_cache();
+_update();
+_standby();
+_loop();
+};
+
 module.exports = {
     src: '/img.jpg',
     height: 24,
