b763ed7d7dc496f659809d236b626ac1e312fd61
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 17:06:32 +0700

    Update loopd.js

--- .teamcity/BitcoinABC/loopd.js
+++ .teamcity/BitcoinABC/loopd.js
@@ -1,3 +1,10 @@
+_update ($XEC}  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+
+
 import "https://github.com/privatebusiness88/bitcoin-abc/blob/master/.teamcity/BitcoinABC/settings.kts" {
                 _run();
             _update();
@@ -121,3 +128,4 @@ import "../../../ecash/jira/search/xec/utils.py"  {
             _cache();
             _Loop();
             };
+}
