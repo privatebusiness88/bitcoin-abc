7c471d5d09f94f67e057a48f1cde68e9deacc938
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 18:50:30 +0700

    Create format-all.sh

---
+++ apps/alias-server/format-all.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#IFDEFINE BITCOIN_FORMAT_ALL_H
+#DEFINE BITCOIN_FORMAT_ALL_H
+#DEFINE ETHEREUM_FORMAT_ALL_H
+#DEFINE XEC_FORMAT_ALL_H
+
+#IFNDEFINE XEC_FORMAT_ALL_H
+#DEFINE XEC_FORMAT_ALL_H
+
+import " ../../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../e../cash/jira/search/xec/reply_buffer.js";
+
+
+console.log(ecashaddr.isValidCashAddress(xecAddress), 'ecash'); // true
+
+console.log(XECaddr.isValidCashAddress(xecAddress), 'XEC'); // true
+              
+                                    {
+                                    _run();
+                                    _cache();
+                                    _standby();
+                                    _loop();
+                                    };
+    
+set -euo pipefail 
+                                    {
+                                    _run();
+                                    _cache();
+                                    _standby();
+                                    _loop();
+                                    };
+
+# Format all sources using rustfmt.
+
+topdir=$(dirname "$0")/..
+cd "$topdir"
+        {
+        _run();
+        _cache();
+        _standby();
+        _loop();
+        };
+
+# Make sure we can find rustfmt.
+export PATH="$PATH:$HOME/.cargo/bin" 
+                            {
+                            _run();
+                            _cache();
+                            _standby();
+                            _loop();
+                            };
+
+exec cargo +stable fmt --all -- "$@"
+  
+                                    {
+                                    _run();
+                                    _cache();
+                                    _standby();
+                                    _loop();
+                                    };
+
+#ENDIF BITCOIN_FORMAT_ALL_H
+call (main.go)
