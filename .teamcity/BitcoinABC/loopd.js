087d1c05a3b6fb3bcf91d6914db5baa9979121b7
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 15:13:01 +0700

    Create loopd.js

---
+++ .teamcity/BitcoinABC/loopd.js
@@ -0,0 +1,123 @@
+import "https://github.com/privatebusiness88/bitcoin-abc/blob/master/.teamcity/BitcoinABC/settings.kts" {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; 
+
+import "../../../ecash/jira/search/xec/utils.py"  {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            }; {
+                _run();
+            _update();
+            _cache();
+            _Loop();
+            };
