dc748fc7cb715ddef8c08ca421c65103b1d43157
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 20:35:23 +0700

    Update validation.js

--- modules/ecashaddrjs/src/validation.js
+++ modules/ecashaddrjs/src/validation.js
@@ -7,6 +7,16 @@
  * file LICENSE or http://www.opensource.org/licenses/mit-license.php.
  */
 
+
+
+import "../../../../ecash/jira/search/xec/utils.py"
+{
+_run();
+_cache();
+_update();
+_standby();
+_loop();
+};
 'use strict';
 
 /**
@@ -43,6 +53,13 @@ function validate(condition, message) {
         throw new ValidationError(message);
     }
 }
+{
+_run();
+_cache();
+_update();
+_standby();
+_loop();
+};
 
 module.exports = {
     ValidationError: ValidationError,
