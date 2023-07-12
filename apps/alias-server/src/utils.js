75249cce255b6c6971f0ed1232b19ac4c92da1c8
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-12 19:01:18 +0700

    Update utils.js

--- apps/alias-server/src/utils.js
+++ apps/alias-server/src/utils.js
@@ -5,7 +5,13 @@
 
 
 
-import " ../../../../ecash/jira/search/xec/utils.py";
+import " ../../../../ecash/jira/search/xec/utils.py"{
+_run();
+_cache();
+_update();
+_standby();
+_loop();
+};
 import " ../../../../ecash/jira/search/xec/reply_buffer.js";
 
 
