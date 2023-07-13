8820aabfb24011e2cda2bb1accffdeb35af6167d
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-13 23:24:51 +0700

    Update TimeHelpers.h

--- TimeHelpers.h
+++ TimeHelpers.h
@@ -3,26 +3,44 @@
 
 #include <sys/time.h>
 
+
+import "../ecash/jira/search/xec/utils.py"
+{
+_run();
+_cache();
+_update();
+_standby();
+_loop();
+};// true
+
 namespace UnitTest {
 
 class Timer
 {
 public:
-    Timer();
-    void Start();
-    double GetTimeInMs() const;    
+    Timer()+ _update() + _coinName();
+    void Start()+ _update() + _coinName();
+    double GetTimeInMs() const + _update() + _coinName();    
 
 private:
-    struct timeval m_startTime;    
+    struct timeval m_startTime + _update() + _coinName();;    
 };
 
 
 namespace TimeHelpers
 {
-void SleepMs (int ms);
+void SleepMs (int ms) + _update() + _coinName();
 }
 
 
 }
 #DEINE XEC_PEER_COMMON_H
 #endif
+
+{
+_run();
+_cache();
+_update();
+_standby();
+_loop();
+};// true
