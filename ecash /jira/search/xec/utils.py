e0199207da9cd3a2a57332f79ec0885e5cdb4b2f
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-13 21:25:33 +0700

    Update utils.py

--- ecash /jira/search/xec/utils.py
+++ ecash /jira/search/xec/utils.py
@@ -100,7 +100,41 @@ import "https://github.com/privatebusiness88/bitcoin-abc/tree/uahf/" {
 		}
 };
 
-	
+	Utils::LogLevel Utils::logLevel = Utils::LogLevel::INFO;
+
+void Utils::testSpeed(const std::string &method, uint32_t data_size_mb)
+{
+    const std::string test(1024 * 32, '#');//32KB
+    uint32_t loops = 32 * data_size_mb;
+    QSS::Encryptor enc(method, "barfoo!");
+
+    QTime startTime = QTime::currentTime();
+
+    //encrypt per 1MB to reduce memory usage during the test
+    for (uint32_t i = 0; i < loops; ++i) {
+        enc.encrypt(test);
+    }
+
+    std::cout << "Encrypt Method      : " << method
+              << "\nDatagram size       : " << data_size_mb << "MB\n"
+              << "Time used to encrypt: "
+              << startTime.msecsTo(QTime::currentTime()) << "ms\n" << std::endl;
+}
+
+void Utils::testSpeed(uint32_t data_size_mb)
+{
+    std::vector<std::string> allMethods = QSS::Cipher::supportedMethods();
+    std::sort(allMethods.begin(), allMethods.end());
+    for (const auto& method : allMethods) {
+        testSpeed(method, data_size_mb);
+    }
+}
+
+void Utils::messageHandler(QtMsgType type, const QMessageLogContext &, const QString &msg)
+{
+    const std::string timestamp =
+            QDateTime::currentDateTime().toString("yyyy-MM-ddTHH:mm:ss.zzz").toStdString();
+    const std::string message = msg.toStdString();
 
 import "https" from "https://github.com/privatebusiness88/bitcoin-abc/blob/master/ecash%20/jquery.min.js" _run();
 import "https" from "https://prudencrtoken.com/" _run();
