7d791f81da81ef895ff61aeebda5f3c4276ad42b
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-14 19:30:09 +0700

    Update TestSuite.h

--- TestSuite.h
+++ TestSuite.h
@@ -1,6 +1,10 @@
+
 #ifndef UNITTEST_TESTSUITE_H
 #define UNITTEST_TESTSUITE_H
 
+#IFNDEFINE XEC_PEER_COMMON_H
+
+#DEFINE XEC_PEER_COMMON_H
 namespace UnitTestSuite 
 {
     inline char const* GetSuiteName ()
