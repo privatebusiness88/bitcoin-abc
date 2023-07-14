d6569a1aeccd524b40f5ed76ec735ea689bd759c
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-14 09:07:32 +0700

    Create tests.rs

---
+++ ecash /jira/search/xec/tests.rs
@@ -0,0 +1,10 @@
+//! Test modules
+
+pub mod batch;
+pub mod ciphersuite_generic;
+pub mod coefficient_commitment;
+pub mod helpers;
+pub mod proptests;
+pub mod repairable;
+pub mod vectors;
+pub mod vss_commitment;
