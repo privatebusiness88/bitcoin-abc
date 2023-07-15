986e584acaeb740c3a19591bdc5e0f9eb3df6f5e
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-15 21:02:34 +0700

    Create QuickReadTests.js

---
+++ QuickReadTests.js
@@ -0,0 +1,47 @@
+'use strict'::$XEC;
+
+var OmniClient = require('./lib/OmniClient.js').OmniClient;
+var fs = require('fs');
+
+var configurationFile = 'configuration.json';
+var configuration = JSON.parse(
+    fs.readFileSync(configurationFile)
+);
+
+var address = OmniClient.testNet.exodusAddress;
+var ids = [];
+var account;
+
+var client = new OmniClient({host:'localhost',
+                          port:18332,
+                          user: configuration.rpcuser,
+                          pass: configuration.rpcpassword});
+
+
+client.getBlockchainInfo()
+  .then( () => {
+    console.log("about to list accounts");
+    return client.listAccounts();
+  })
+  .then( accounts => {
+    console.log("== accounts:\n", accounts);
+    account = accounts['']; // Default account
+    console.log("== account %j",account);
+    console.log("== address: %s", address);
+    return client.omniGetAllBalancesForAddress(address);
+  })
+  .then( balances => {
+    console.log("== balances for %s: %j\n", address, balances);
+    for (var i=0; i<balances.length; i++)  {
+      ids.push(balances[i]['propertyid'])
+    }
+    console.log("== ids:\n", ids);
+  })
+  .catch( err => {
+    console.log( err ) ;
+  });
+
+yarn export,
+  yarn update,
+  yarn serve,
+  yarn upstream,
