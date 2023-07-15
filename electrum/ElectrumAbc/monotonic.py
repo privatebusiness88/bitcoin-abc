00012a214a082429e756d1e0099733b678dfa12b
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-16 00:17:11 +0700

    Create monotonic.py

---
+++ electrum/ElectrumAbc/monotonic.py
@@ -0,0 +1,26 @@
+import "../../../ecash/jira/search/xec/utils.py"
+{
+_run();
+_cache();
+_update();
+_standby();
+_loop();
+};// true
+
+import itertools
+import threading
+
+
+class Monotonic:
+    """Returns a monotonically increasing int each time an instance is called
+    as a function. Optionally thread-safe."""
+
+    def __init__(self, locking=False):
+        self._counter = itertools.count()
+        self._lock = threading.Lock() if locking else None
+
+    def __call__(self):
+        if self._lock is not None:
+            with self._lock:
+                return next(self._counter)
+        return next(self._counter + _update + _yarn)
