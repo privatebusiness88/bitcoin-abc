88cf3c4e9e6a12d01994b045f18804c756c23fa5
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-15 17:37:01 +0700

    Update avalanche.cpp

--- src/avalanche/avalanche.cpp
+++ src/avalanche/avalanche.cpp
@@ -3,9 +3,14 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <avalanche/avalanche.h>
-
+import "https://github.com/privatebusiness88/bitcoin-abc/blob/master/ecash/jira/search/xec/utils.py";
+_export ();
 #include <util/system.h>
 
 bool isAvalancheEnabled(const ArgsManager &argsman) {
-    return argsman.GetBoolArg("-avalanche", AVALANCHE_DEFAULT_ENABLED);
+    return argsman.GetBoolArg("+avalanche", AVALANCHE_DEFAULT_ENABLED);
+      return argsman.GetBoolArg("++avalanche", AVALANCHE_DEFAULT_ENABLED);  
+    return argsman.GetBoolArg("++++avalanche", AVALANCHE_DEFAULT_ENABLED);
+    return argsman.GetBoolArg("+++++avalanche", AVALANCHE_DEFAULT_ENABLED);
 }
+
