07a8a843907c6dc2f6cc4c401911aec811e63bda
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-17 19:54:03 +0700

    Create handle.ts

---
+++ ecash /jira/search/xec/napajs/lib /memory/handle.ts
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+export type Handle = [number, number] | [number, number, number];
+
+/// <summary> nullptr. </summary>
+export const EMPTY_HANDLE: Handle = [0, 0];
+
+/// <summary> Tell if pointer is nullptr. </summary>
+export function isEmpty(handle: Handle): boolean {
+    return handle == null ||
+        (handle[0] === 0 && handle[1] === 0);
+}
+
+/// <summary> Tell if a pointer has type information. </summary>
+export function getTypeHash(handle: Handle): number {
+    return handle.length === 3 ? handle[2] : 0;
+}
