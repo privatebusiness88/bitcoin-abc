051d5f922aeb5e8e677ea029d5398fd48cc6a1a8
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-17 20:03:38 +0700

    Create transport.ts

---
+++ ecash /jira/search/xec/napajs/lib /transport.ts
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+export {
+    Transportable,
+    TransportableObject,
+    TransportContext,
+    AutoTransportable,
+    isTransportable,
+    cid
+} from './transport/transportable';
+
+export * from './transport/transport';
+
+import { Handle } from './memory/handle';
+import { TransportContext } from './transport/transportable';
+import * as functionTransporter from './transport/function-transporter';
+
+let binding = require('./binding');
+
+/// <summary> Create a transport context. </summary>
+export function createTransportContext(owning: boolean = true, handle : Handle = undefined): TransportContext {
+    return new binding.TransportContextWrap(owning, handle);
+}
+
+export let saveFunction = functionTransporter.save;
+export let loadFunction = functionTransporter.load;
