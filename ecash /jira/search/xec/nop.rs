4c2175b5d6f5dd1708260aa9193cba74a0c4120f
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-18 18:56:23 +0700

    Create nop.rs

---
+++ ecash /jira/search/xec/nop.rs
@@ -0,0 +1,45 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use io_uring::{opcode, IoUring};
+
+struct TaskQueue(usize);
+
+impl TaskQueue {
+    pub fn want(&self) -> bool {
+        self.0 != 0
+    }
+
+    pub fn pop(&mut self) {
+        self.0 -= 1;
+    }
+}
+
+fn bench_normal(c: &mut Criterion) {
+    let mut io_uring = IoUring::new(16).unwrap();
+
+    c.bench_function("normal", |b| {
+        b.iter(|| {
+            let mut queue = TaskQueue(128);
+
+            while queue.want() {
+                {
+                    let mut sq = io_uring.submission();
+                    while queue.want() {
+                        unsafe {
+                            match sq.push(&black_box(opcode::Nop::new()).build()) {
+                                Ok(_) => queue.pop(),
+                                Err(_) => break,
+                            }
+                        }
+                    }
+                }
+
+                io_uring.submit_and_wait(16).unwrap();
+
+                io_uring.completion().map(black_box).for_each(drop);
+            }
+        });
+    });
+}
+
+criterion_group!(squeue, bench_normal);
+criterion_main!(squeue);
