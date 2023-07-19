30f658c9132f9d390a662f1c873ca3563f48840f
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-19 11:09:41 +0700

    Create index.ts

---
+++ ecash /jira/search/xec/deploy/index.ts
@@ -0,0 +1,6 @@
+import "./liquidityMiningManager.ts";
+import "./TimeLockNonTransferablePool.ts";
+import "./View.ts";
+import "./TestFaucetToken.ts";
+import "./TokenSaver.ts";
+import "./full.ts";
