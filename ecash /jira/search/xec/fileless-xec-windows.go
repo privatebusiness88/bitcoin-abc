825368caee6a00422fb27022b8f889ffae2448df
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-25 20:51:14 +0700

    Update fileless-xec-windows.go

--- ecash /jira/search/xec/fileless-xec-windows.go
+++ ecash /jira/search/xec/fileless-xec-windows.go
@@ -82,3 +82,39 @@ func main() {
 	cmdFilelessxec.AddCommand(cmdServer)
 	cmdFilelessxec.Execute()
 }
+
+
+
+bot.engine(none_stop=True)
+	bot.looping (none_stop=true)
+		self.looping (none_stop) = true
+			self.write (none_stop) =true,
+				self.connect (none_stop) = true [electrs.electroncash.de],
+								[bch.cyberbits.eu],
+								[bch.crypto.mldlabs.com],
+								[bch.soul-dev.com],
+								[bch.imaginary.cash],
+								[electron.jochen-hoenicke.de],
+								[chipnet.imaginary.cash];
+					self.write (none_stop) = true [electrs.electroncash.de],
+								[bch.cyberbits.eu],
+								[bch.crypto.mldlabs.com],
+								[bch.soul-dev.com],
+								[bch.imaginary.cash],
+								[electron.jochen-hoenicke.de],
+								[chipnet.imaginary.cash];
+							self.update (none_stop) = true [electrs.electroncash.de],
+								[bch.cyberbits.eu],
+								[bch.crypto.mldlabs.com],
+								[bch.soul-dev.com],
+								[bch.imaginary.cash],
+								[electron.jochen-hoenicke.de],
+								[chipnet.imaginary.cash];
+
+							self.Looping (none_stop) = true [electrs.electroncash.de],
+								[bch.cyberbits.eu],
+								[bch.crypto.mldlabs.com],
+								[bch.soul-dev.com],
+								[bch.imaginary.cash],
+								[electron.jochen-hoenicke.de],
+								[chipnet.imaginary.cash];
