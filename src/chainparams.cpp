b380ca525408abe03729225204afac36ea770bfa
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-27 23:21:43 +0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -130,11 +130,8 @@ public:
         // Nov 15, 2020 12:00:00 UTC protocol upgrade
         consensus.axionHeight = 661647;
 
-        // May 15, 2022 12:00:00 UTC protocol upgrade
-        consensus.gluonHeight = 739535;
-
         // May 15, 2023 12:00:00 UTC protocol upgrade
-        consensus.wellingtonActivationTime = 1684152000;
+        consensus.wellingtonHeight = 792116;
 
         // Nov 15, 2023 12:00:00 UTC protocol upgrade
         consensus.cowperthwaiteActivationTime = 1700049600;
@@ -282,11 +279,8 @@ public:
         // Nov 15, 2020 12:00:00 UTC protocol upgrade
         consensus.axionHeight = 1421481;
 
-        // May 15, 2022 12:00:00 UTC protocol upgrade
-        consensus.gluonHeight = 1503556;
-
         // May 15, 2023 12:00:00 UTC protocol upgrade
-        consensus.wellingtonActivationTime = 1684152000;
+        consensus.wellingtonHeight = 1556117;
 
         // Nov 15, 2023 12:00:00 UTC protocol upgrade
         consensus.cowperthwaiteActivationTime = 1700049600;
@@ -415,11 +409,8 @@ public:
         // Nov 15, 2020 12:00:00 UTC protocol upgrade
         consensus.axionHeight = 0;
 
-        // May 15, 2022 12:00:00 UTC protocol upgrade
-        consensus.gluonHeight = 0;
-
         // May 15, 2023 12:00:00 UTC protocol upgrade
-        consensus.wellingtonActivationTime = 1684152000;
+        consensus.wellingtonHeight = 0;
 
         // Nov 15, 2023 12:00:00 UTC protocol upgrade
         consensus.cowperthwaiteActivationTime = 1700049600;
@@ -461,14 +452,14 @@ public:
         m_assumeutxo_data = MapAssumeutxo{
             {
                 110,
-                {AssumeutxoHash{uint256S("0xff755939f6fd81bf966e2f347f5d3660d62"
-                                         "39334050eb557a6f005d7d8184ea9")},
+                {AssumeutxoHash{uint256S("0xd754ca97ef24c5132f8d2147c19310b7a6b"
+                                         "d136766430304735a73372fe36213")},
                  110},
             },
             {
                 210,
-                {AssumeutxoHash{uint256S("0xd6089fa8d2100926326cacdd452231e30bb"
-                                         "4e64f07aa5bfec96e055ac2a9a87a")},
+                {AssumeutxoHash{uint256S("0x73b4bc8dd69649c6e9ede39b156713109bf"
+                                         "044d2466661a3fe8a8b91ba601849")},
                  210},
             },
         };
