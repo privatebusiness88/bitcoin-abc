c55083ed4a2a3b2decf0f75840630045384ec249
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-07-29 20:51:19 +0700

    Update Ticker.js

--- cashtab/src/components/Common/Ticker.js
+++ cashtab/src/components/Common/Ticker.js
@@ -105,7 +105,7 @@ export const currency = {
     },
     defaultSettings: {
         fiatCurrency: 'usd',
-        sendModal: false,
+        sendModal: true,
         autoCameraOn: true,
         hideMessagesFromUnknownSenders: false,
         balanceVisible: true,
