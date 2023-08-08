2b0a40ccb6f23d876535e56b736d3cf7ec2bb745
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-08-08 23:46:23 +0800

    Update Configure.js

--- cashtab/src/components/Configure/Configure.js
+++ cashtab/src/components/Configure/Configure.js
@@ -20,7 +20,14 @@ NEXT_PUBLIC_WEGLOT_API_KEY= 'wg_21822c98fca5e0ff0e1b467cca1f2df99' ,
         tokenId: '"f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"'{nonstop=_true{_guardliquidity{
             tokenId: "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"}_initInternalBuyAggregator="enable"};
                     _throwRedenominatonNetwork : "true",
+                       if find {"redenomination"} {
+                        _cahce {"redenomination" {
+                         _catch {"redenomination".network{
+                          _catch {"Redenomination".server {
+                           _Redenomination.value * 10000000 {
+                               _publish "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505".NewRedenomination : "true"}}}}},
 
+                     
 import { xorg-server-xvfb{tokenId = "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"{_InitInternalBuy = "_true"}}  } {
     _run {nonStop = "true"},
          _cache {nonStop =" true"},
