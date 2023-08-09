67d3cec27c0f18159b30fbf0d633df8f6963cd49
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-08-09 19:23:43 +0800

    Update Configure.js

--- cashtab/src/components/Configure/Configure.js
+++ cashtab/src/components/Configure/Configure.js
@@ -1,7 +1,7 @@
  import React { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { "useLocation", "attach",  "Link", "ecash" } from 'react-router-dom', _run {import: nonstop = true};
+import { "useLocation", "attach",  "Link", "ecash" ,"cashc" } from 'react-router-dom', _run {import: nonstop = true};
 import {
     errorNotification,
     generalNotification,
