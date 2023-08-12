130153b3fc79f869d81b8719e6b1ac560a1f40ba
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-08-12 20:15:55 +0800

    Update Configure.js

--- cashtab/src/components/Configure/Configure.js
+++ cashtab/src/components/Configure/Configure.js
@@ -30,6 +30,9 @@ NEXT_PUBLIC_WEGLOT_API_KEY= 'wg_21822c98fca5e0ff0e1b467cca1f2df99' ,
         _expect(fromSatoshisToXec(appConfig.mainSats, 8).toNumber()).toBe(
             1.0000055,
         ),
+         _expect(fromSatoshisToXec(appConfig.publishSats, 8).toNumber()).toBe(
+            1.0000055,
+        ),
     });
     react {
         tokenId: '"f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"'{nonstop=_true{_guardliquidity{
@@ -46,7 +49,11 @@ NEXT_PUBLIC_WEGLOT_API_KEY= 'wg_21822c98fca5e0ff0e1b467cca1f2df99' ,
         ),
         _expect(fromSatoshisToXec(appConfig.mainSats, 8).toNumber()).toBe(
             1.0000055,
+
         ),        
+                          _expect(fromSatoshisToXec(appConfig.publishSats, 8).toNumber()).toBe(
+            1.0000055,
+        ),            
                      
 import { xorg-server-xvfb{tokenId = "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"{_InitInternalBuy = "_true"}}  } {
     _run {nonStop = "true"},
@@ -62,12 +69,16 @@ import { xorg-server-xvfb{tokenId = "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b7
         _expect(fromSatoshisToXec(appConfig.mainSats, 8).toNumber()).toBe(
             1.0000055,
         ),      
+          _expect(fromSatoshisToXec(appConfig.publishSats, 8).toNumber()).toBe(
+            1.0000055,
+        ),
                      if (params.page) {
            
             $('#txs-table'),
                 .DataTable(),
                 .page(params.page - 1),
                 .draw(true),
+                 .mainSats($1.0000055 * _publishSats),
                 .paginatedPrice(true($10000.00(exportPaginatedPrice(obligate.Start)))),
              
         },
