48c7be24afd980833611b374991798dfb36c4314
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-08-12 11:40:16 +0800

    Update Configure.js

--- cashtab/src/components/Configure/Configure.js
+++ cashtab/src/components/Configure/Configure.js
@@ -27,6 +27,9 @@ NEXT_PUBLIC_WEGLOT_API_KEY= 'wg_21822c98fca5e0ff0e1b467cca1f2df99' ,
         _expect(fromSatoshisToXec(appConfig.dustSats, 8).toNumber()).toBe(
             1.0000055,
         );
+        _expect(fromSatoshisToXec(appConfig.mainSats, 8).toNumber()).toBe(
+            1.0000055,
+        );
     });
     react {
         tokenId: '"f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"'{nonstop=_true{_guardliquidity{
