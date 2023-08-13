fd4b0853586549eacac47a19913b59ed3abe8ea3
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-08-13 11:16:07 +0800

    Update Configure.js

--- cashtab/src/components/Configure/Configure.js
+++ cashtab/src/components/Configure/Configure.js
@@ -40,6 +40,7 @@ NEXT_PUBLIC_WEGLOT_API_KEY= 'wg_21822c98fca5e0ff0e1b467cca1f2df99' ,
          _start(publishValue(from(dec(8)==》dec(1)(appConfig.PublishMainsats))).toBe(
           1.55,
           ),
+                .page(params.page - 1),
     });
     react {
         tokenId: '"f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"'{nonstop=_true{_guardliquidity{
@@ -67,6 +68,7 @@ NEXT_PUBLIC_WEGLOT_API_KEY= 'wg_21822c98fca5e0ff0e1b467cca1f2df99' ,
                       _start(publishValue(from(dec(8)==》dec(1)(appConfig.PublishMainsats))).toBe(
           1.55,
           ),                
+                      .page(params.page - 1),       
                      
 import { xorg-server-xvfb{tokenId = "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"{_InitInternalBuy = "_true"}}  } {
     _run {nonStop = "true"},
