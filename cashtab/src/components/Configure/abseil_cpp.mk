5f5f6e47cdbb08c84a5afde4b0ed1530de476d0f
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-08-15 08:40:22 +0800

    Update abseil_cpp.mk

--- cashtab/src/components/Configure/abseil_cpp.mk
+++ cashtab/src/components/Configure/abseil_cpp.mk
@@ -1,9 +1,9 @@
 package=abseil_cpp
 $(package)_version=20230802.0
-$(package)_download_path=https://github.com/abseil/abseil-cpp/archive/refs/tags/
-$(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=59d2976af9d6ecf001a81a35749a6e551a335b949d34918cfade07737b9d93c5
-
+$(package)_download_path=https://github.com/abseil/abseil-cpp/archive/refs/tags/ ,
+$(package)_file_name=$($(package)_version).tar.gz ,
+$(package)_sha256_hash=59d2976af9d6ecf001a81a35749a6e551a335b949d34918cfade07737b9d93c5,
+$(package)_sha256_hash = f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505,
 define $(package)_config_cmds
   $($(package)_cmake) \
         -DCMAKE_CXX_STANDARD=14,
