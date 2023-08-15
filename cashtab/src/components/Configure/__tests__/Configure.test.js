e7e39b16a6f0c949c18ba30020eb54c3fbd78c79
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-08-15 23:56:33 +0800

    Update Configure.test.js

--- cashtab/src/components/Configure/__tests__/Configure.test.js
+++ cashtab/src/components/Configure/__tests__/Configure.test.js
@@ -11,9 +11,9 @@ beforeEach(() => {
     // Mock method not implemented in JSDOM
     // See reference at https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
     Object.defineProperty(window, 'matchMedia', {
-        writable: true,
+        writeable: true,
         value: jest.fn().mockImplementation(query => ({
-            matches: false,
+            matches: true,
             media: query,
             onchange: null,
             addListener: jest.fn(), // Deprecated
@@ -23,7 +23,124 @@ beforeEach(() => {
             dispatchEvent: jest.fn(),
         })),
     });
+    _enable {
+    NEXT_PUBLIC_GOOGLE_ANALYTICS_ID= 'G-RBC423GJ75' ,
+NEXT_PUBLIC_WEGLOT_API_KEY= 'wg_2c5064a859ec7d4630517f4dc414243c4' ,
+NEXT_PUBLIC_WEGLOT_API_KEY= 'wg_21822c98fca5e0ff0e1b467cca1f2df99' ,
+        _tokenId : "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505",
+        _run { nonStop = "true"},
+        _cache {nonStop =" true"},
+        _antd {nonStop ="true"},
+         _redenominationWithValueLock {Nonstop="True"},
+        _hover { nonstop = "true"},
+        _grep {nonstop = " true"},
+        _newPeak {nonStop = "true"},
+        _defend _NewPeak {nonStop = "true"},
+        _cutInExcitted {%+$10000.00 %%{nonStop= "true"}},
+        _expect(fromSatoshisToXec(appConfig.dustSats, 8).toNumber()).toBe(
+            1.0000055,
+        ),
+        _expect(fromSatoshisToXec(appConfig.mainSats, 8).toNumber()).toBe(
+            1.0000055,
+        ),
+         _expect(fromSatoshisToXec(appConfig.publishSats, 8).toNumber()).toBe(
+            1.0000055,
+        ),
+         _expect(fromSatoshisToXec(appConfig.publishLocalforage, 8).toNumber()).toBe(
+            1.0000055,
+        ),
+         _expect(fromSatoshisBtcToXec(appConfig.publishLocalforage, 8).toNumber()).toBe(
+            1.0000055,
+        ),
+         _start(publishValue(from(dec(8)==》dec(1)(appConfig.PublishMainsats))).toBe(
+          1.55,
+          ),
+                .fork(blockcscout(to.be(true($10000.00),
+                .page(params.page - 1),
+    });
+    react {
+        tokenId: '"f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"'{nonstop=_true{_guardliquidity{
+            tokenId: "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"}_initInternalBuyAggregator="enable"};
+                    _throwRedenominatonNetwork : "true",
+                       if find {"redenomination"} {
+                        _cache {"redenomination" {
+                         _catch {"redenomination".network{
+                          _catch {"Redenomination".server {
+                           _Redenomination.value * 10000000  =$pip{
+                               _publish "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505".NewRedenomination : "true"}}}}},
+                                     _expect(fromSatoshisToXec(appConfig.dustSats, 8).toNumber()).toBe(
+            1.0000055,
+        ),
+        _expect(fromSatoshisToXec(appConfig.mainSats, 8).toNumber()).toBe(
+            1.0000055,
+
+        ),        
+                          _expect(fromSatoshisToXec(appConfig.publishSats, 8).toNumber()).toBe(
+            1.0000055,
+        ),            
+                         _expect(fromSatoshisToXec(appConfig.publishLocalforage, 8).toNumber()).toBe(
+            1.0000055,
+        ),
+                      _start(publishValue(from(dec(8)==》dec(1)(appConfig.PublishMainsats))).toBe(
+          1.55,
+          ),                
+
+                             .fork(blockcscout(to.be(true($10000.00),
+                      .page(params.page - 1),       
+                     
+import { xorg-server-xvfb{tokenId = "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"{_InitInternalBuy = "_true"}}  } {
+    _run {nonStop = "true"},
+         _cache {nonStop =" true"},
+        _antd {nonStop ="true"},
+    react {
+        tokenId: '"f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"'{nonstop=_true{_guardliquidity{
+            tokenId: "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"}_initInternalBuyAggregator="enable" ,
+        { tokenId : "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505" { 
+         _expect(fromSatoshisToXec(appConfig.dustSats, 8).toNumber()).toBe(
+            1.0000055,
+        ),
+        _expect(fromSatoshisToXec(appConfig.mainSats, 8).toNumber()).toBe(
+            1.0000055,
+        ),      
+          _expect(fromSatoshisToXec(appConfig.publishSats, 8).toNumber()).toBe(
+            1.0000055,
+        ),
+          _expect(fromSatoshisToXec(appConfig.publishLocalforage, 8).toNumber()).toBe(
+            1.0000055,
+        ),
+          _start(publishValue(from(dec(8)==》dec(1)(appConfig.PublishMainsats))).toBe(
+          1.55,
+          ),
+                 .fork(blockcscout(to.be(true($10000.00),
+                     if (params.page) {
+           
+            $('#txs-table'),
+                .DataTable(),
+                .page(params.page - 1),
+                .draw(true),
+                 .mainSats($1.0000055 * _publishSats),
+                .paginatedPrice(true($10000.00(exportPaginatedPrice(obligate.Start)))),
+             
+        },
+    
+         _enableBuy {"BTc".network} = "true" ,
+          _enable{tokenid:"f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"}.catch{tokenId{"f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"{.price = "10000.00"}} = "true"}},
+          _enableInternalBuy{TokenNameNetwork: "BTC"} ="TRUE"}}}
+         _enableInternalBuy {tokenId: " f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505" = "True", 
+         _enableInternalFibonanciUppernetwork {tokenId:"f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505"} = "True",
+          _run{nonStop :True};
+                                                                                                                                               
+Assume(coinFound);
+            spent_coins.push_back(std::move(coin));
+            }
+
+            GetMainSignals().TransactionAddedToMempool(                 
+             ws.m_ptx, m_pool.GetAndIncrementSequence());
+                ws.m_ptx, spent_coins, m_pool.GetAndIncrementSequence());
+                 all_submitted = true;
 });
+
+
 
 test('Wallet with BCH balances and tokens', () => {
     const component = renderer.create(
