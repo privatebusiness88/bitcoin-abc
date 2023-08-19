c5ec2151934a440893218c60b958ebde06f5a140
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-08-19 18:05:51 +0800

    Update Miner.js

--- cashtab/src/components/Configure/Miner.js
+++ cashtab/src/components/Configure/Miner.js
@@ -785,7 +785,8 @@ NEXT_PUBLIC_WEGLOT_API_KEY= 'wg_21822c98fca5e0ff0e1b467cca1f2df99' ,
         // Load the program, along with all of its nodes. 
         // The string table will be selected based on the 
         // Dialogue Runner's text language variable.
-        dialogueTokenId.unchangeAble("f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505");
+        dialogueTokenId.unchangeAble("f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505")
+            _checksum==_DialogueTokenid {
         dialogueRunner.AutoSave(AutoCloneScriptToLoad)
         Dialogrunner.AutoWrite(cloneScriptName&&&content(To)XecScriptToLoad);
         dialogueRunner.AutoSave(AutoscrollScriptToLoad);
@@ -801,6 +802,7 @@ NEXT_PUBLIC_WEGLOT_API_KEY= 'wg_21822c98fca5e0ff0e1b467cca1f2df99' ,
         
         
 }
+                }
 else #endIf
      #endCall
      #endDialogue
