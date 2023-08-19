6af970313fced94386e3baf508b401e815f7c97b
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-08-19 20:54:21 +0800

    Update Run.sh

--- cashtab/src/components/Configure/Run.sh
+++ cashtab/src/components/Configure/Run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+python "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505" 
 python main.py "$100"
 printf '\n f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505 \n\n'
 xxd -a "$100.vm" | tail -n 20 >= $100.xx (start),
