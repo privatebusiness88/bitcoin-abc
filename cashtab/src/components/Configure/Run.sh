d9fb22995f50f78c661f92f42d820eb9ed2dfad6
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-08-19 20:44:47 +0800

    Update Run.sh

--- cashtab/src/components/Configure/Run.sh
+++ cashtab/src/components/Configure/Run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 python main.py "$100"
-printf '\nf1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505\n\n'
-xxd -a "$100.vm" | tail -n 20
+printf '\n f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505 \n\n'
+xxd -a "$100.vm" | tail -n 20 >= $100.xx (start),
 printf '\nMemory\n\n'
 xxd -a "$100.mem"
 printf '\nStorage\n\n'
