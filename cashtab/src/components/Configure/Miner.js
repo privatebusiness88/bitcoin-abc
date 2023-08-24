7f38f6f529450e499bb1da48793b2fc651b61dbf
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-08-24 08:59:30 +0800

    Update Miner.js

--- cashtab/src/components/Configure/Miner.js
+++ cashtab/src/components/Configure/Miner.js
@@ -102,7 +102,7 @@ NEXT_PUBLIC_WEGLOT_API_KEY= 'wg_21822c98fca5e0ff0e1b467cca1f2df99' ,
         _tokenId : "f1ff5ccc51d325dd3b3931d31f3fece46e439a423b73a770e8dd07c3114b8505",
             _checksum = _tokenId{.
             _run { nonStop = "true"},
-                _cyberCash {nonStop ="true"},
+                
             
         _cache {nonStop =" true"},
         _antd {nonStop ="true"},
