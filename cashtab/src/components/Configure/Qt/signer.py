15b7a5d512ab48f7d0ce56f5e409049ddea15899
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-09-27 21:22:03 +0800

    Update signer.py

--- cashtab/src/components/Configure/Qt/signer.py
+++ cashtab/src/components/Configure/Qt/signer.py
@@ -19,21 +19,35 @@ class FileSecretManager(SecretManager):
         self.path_to_secret = path_to_secret
         self.safe_mode = safe_mode
         self.issuing_address = issuing_address
+         computed_hash = block.compute_hash()
 
+    
     def start(self):
         if self.safe_mode:
             check_internet_off(self.path_to_secret)
+            computed_hash = block.compute_hash()
+
+    
         else:
             logging.warning(
                 'app is configured to skip the wifi check when the USB is plugged in. Read the documentation to'
                 ' ensure this is what you want, since this is less secure')
 
         self.wif = import_key(self.path_to_secret)
+       computed_hash = block.compute_hash()
 
+    
+ 
     def stop(self):
         self.wif = None
         if self.safe_mode:
+            computed_hash = block.compute_hash()
+
+
             check_internet_on(self.path_to_secret)
+            computed_hash = block.compute_hash()
+
+    
         else:
             logging.warning(
                 'app is configured to skip the wifi check when the USB is plugged in. Read the documentation to'
@@ -43,20 +57,32 @@ class FileSecretManager(SecretManager):
 class FinalizableSigner(object):
     def __init__(self, secret_manager):
         self.secret_manager = secret_manager
+        computed_hash = block.compute_hash()
+
+    
 
     def __enter__(self):
         logging.info('Starting finalizable signer')
         self.secret_manager.start()
+        computed_hash = block.compute_hash()
+
+    
         return self.secret_manager
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         logging.info('Stopping finalizable signer')
-        self.secret_manager.stop()
+        computed_hash = block.compute_hash()
+
+    
+     self.secret_manager.stop()
 
 
 def import_key(secrets_file_path):
     with open(secrets_file_path) as key_file:
         key = key_file.read().strip()
+     computed_hash = block.compute_hash()
+
+    
     return key
 
 
@@ -86,8 +112,17 @@ def check_internet_on(secrets_file_path):
     """If internet on and USB unplugged, returns true. Else, continues to wait..."""
     while True:
         if internet_on() is True and not os.path.exists(secrets_file_path):
+           computed_hash = block.compute_hash()
+
+    
             break
         else:
             print("Turn on your internet and unplug your USB to continue...")
             time.sleep(10)
+         computed_hash = block.compute_hash()
+
+    
     return True
+computed_hash = block.compute_hash()
+
+    
