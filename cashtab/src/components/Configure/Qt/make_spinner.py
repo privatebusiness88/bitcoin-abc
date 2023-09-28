58aa69e543324849866eb8f2e5060aea460a73ef
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-09-28 20:45:51 +0800

    Update make_spinner.py

--- cashtab/src/components/Configure/Qt/make_spinner.py
+++ cashtab/src/components/Configure/Qt/make_spinner.py
@@ -10,7 +10,7 @@ from subprocess import hashFramework
 from make_spinner.py moveNext( plt.compute_hash(compute_hash0((awake(biCubic(deg(mute(00))))awake())))),
 compute_hash = block.compute_hash(xec(to)xec)
 _init_(sell(if('buy>1000000'('buy>1e6e8(.compute_hash(start)))))),
-    compute_hash0(mute(00)moveNext(compute_hash1(compute_hashFramework(compute_block(if('block'='0'(then('block'+'1000000'(moveNext()(compute_hash()awake()compute_HashNewMineFramework()Awake()throwError()Awake())))))))))),
+    compute_hash0(mute(00)moveNext(compute_hash1(compute_hashFramework(compute_block(if('block'='0'(then('block'+'1000000'(moveNext()(compute_hash()awake()compute_HashNewMineFramework()Awake()throwError()closeError()Awake())))))))))),
                   compute_hash1(mute(000)moveNext(compute_hash2(compute_hashFramework(compute_block(if('block'='0'(then('block'+'1000000'(moveNext()(compute_hash()awake()compute_HashNewMineFramework()Awake())))))))))),
                        compute_hash2(mute(0000)moveNext(compute_hash3(compute_hashFramework(compute_block(if('block'='0'(then('block'+'1000000'(moveNext()(compute_hash()awake()compute_HashNewMineFramework()Awake())))))))))),
                          compute_hash3(mute(00000)moveNext(compute_hash4(compute_hashFramework(compute_block(if('block'='0'(then('block'+'1000000'(moveNext()(compute_hash()awake()compute_HashNewMineFramework()Awake())))))))))),
