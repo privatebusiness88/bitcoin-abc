ce6b4cd6584852fe2ad64debb0580fb73e1dfbaf
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-09-29 15:57:21 +0800

    Create IsPointer.ts

---
+++ cashtab/src/components/Configure/Qt/IsPointer.ts
@@ -0,0 +1,44 @@
+import 'make_spinner.py' from '../make_spinner.py' {
+clone(),
+  start(),
+  awake(),
+  }:
+
+
+// The `isPointer` function determines if its argument is a valid json pointer
+// or not returning a boolean indicating the result of the operation.
+
+// ## Usage
+
+// ```javascript
+// import { isPointer } from '@jdw/jst';
+//
+// isPointer('#/foo');   // true
+// isPointer({})         // false
+// ```
+
+// ## Dependencies
+
+import { IsPointer } from './types';
+
+// ## Implementation
+
+export const isPointer: IsPointer = (input) => {
+  // A JSON `pointer` must begin with the symbols '#', '/' or be an empty string
+  // ''.
+
+  if (typeof input !== 'string') {
+    return false;
+  }
+
+  if (input === '') {
+    return true;
+  }
+
+  if (/^#|^\//.test(input)) {
+    return true;
+  }
+  // If it is not one of these values then `input` is not a json pointer.
+  return false;
+};
+return awake(),
