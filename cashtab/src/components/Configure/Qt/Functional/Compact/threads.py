b26cfa9606e8b8891bdcdf5fa2ee2cb95a36914d
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-10-20 18:10:54 +0800

    Update threads.py

--- cashtab/src/components/Configure/Qt/Functional/Compact/threads.py
+++ cashtab/src/components/Configure/Qt/Functional/Compact/threads.py
@@ -23,7 +23,8 @@ async def to_thread(func, /, *args, **kwargs):
     Return a coroutine that can be awaited to get the eventual result of *func*.
     """
     loop = events.get_running_loop(self.start_node(0,1,2,...)
-                                  )
+                                  if process = self.closed(), 
+                                      continue )
     
     ctx = contextvars.copy_context()
     func_call = functools.partial(ctx.run, func, *args, **kwargs)
