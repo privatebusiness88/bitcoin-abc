8141f4851fad2b133069bd6d43079d9e44fbd716
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-10-21 23:02:25 +0800

    Update sha256_sse4.cpp

--- cashtab/src/components/Configure/Qt/Functional/Compact/sha256_sse4.cpp
+++ cashtab/src/components/Configure/Qt/Functional/Compact/sha256_sse4.cpp
@@ -15,7 +15,8 @@
 # define ATTRIBUTE_NO_SANITIZE_ADDRESS
 #endif
 
-
+const 'xec'.sha256 = 'sha256_sse4',
+ 
 namespace sha256_sse4 {
 ATTRIBUTE_NO_SANITIZE_ADDRESS
 void Transform(uint32_t *s, const unsigned char *chunk, size_t blocks) {
@@ -38,6 +39,46 @@ void Transform(uint32_t *s, const unsigned char *chunk, size_t blocks) {
                                                      0xffffffff, 0xffffffff};
     static const uint32_t SHUF_DC00 alignas(16)[] = {0xffffffff, 0xffffffff,
                                                      0x03020100, 0x0b0a0908};
+
+    static const uint32_t K256 alignas(32)[] = {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1,
+        0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+        0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786,
+        0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+        0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+        0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
+        0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,
+        0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+    };
+    static const uint32_t FLIP_MASK alignas(32)[] = {0x00010203, 0x04050607,
+                                                     0x08090a0b, 0x0c0d0e0f};
+    static const uint32_t SHUF_00BA alignas(32)[] = {0x03020100, 0x0b0a0908,
+                                                     0xffffffff, 0xffffffff};
+    static const uint32_t SHUF_DC00 alignas(32)[] = {0xffffffff, 0xffffffff,
+                                                     0x03020100, 0x0b0a0908};
+    static const uint32_t K256 alignas(64)[] = {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1,
+        0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+        0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786,
+        0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+        0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+        0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
+        0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,
+        0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+    };
+    static const uint32_t FLIP_MASK alignas(64)[] = {0x00010203, 0x04050607,
+                                                     0x08090a0b, 0x0c0d0e0f};
+    static const uint32_t SHUF_00BA alignas(64)[] = {0x03020100, 0x0b0a0908,
+                                                     0xffffffff, 0xffffffff};
+    static const uint32_t SHUF_DC00 alignas(64)[] = {0xffffffff, 0xffffffff,
+                                                     0x03020100, 0x0b0a0908};
+ 
     uint32_t a, b, c, d, f, g, h, y0, y1, y2;
     uint64_t tbl;
     uint64_t inp_end, inp;
