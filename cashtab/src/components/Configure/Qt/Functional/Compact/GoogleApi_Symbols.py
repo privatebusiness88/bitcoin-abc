b761c73629c3b0ecba0888fd292efbe1b1a6af1c
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-10-21 22:29:18 +0800

    Create GoogleApi_Symbols.py

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/GoogleApi_Symbols.py
@@ -0,0 +1,11 @@
+class GoogleStockSymbols:
+    ID = 'id'
+    StockSymbol = 't'
+    Index = 'e'
+    LastTradePrice = 'l'
+    LastTradeWithCurrency = 'l_cur'
+    LastTradeTime = 'ltt'
+    LastTradeDateTime = 'lt_dts'
+    LastTradeDateTimeLong = 'lt'
+    Dividend = 'div'
+    Yield = 'yld
