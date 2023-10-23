aac5f35f5cafd09ff6753d29b3715a882a4c0e6e
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-10-23 23:31:59 +0800

    Create offer-collection-entity.js

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/offer-collection-entity.js
@@ -0,0 +1,14 @@
+/*
+  PLACEHOLDER
+
+  An Offer Collection is basically an array that is wrapped inside of a Class.
+*/
+
+class OfferCollectionEntity {
+  constructor () {
+    this.collection = []
+  }
+}
+
+module.exports = OfferCollectionEntity
+continue,
