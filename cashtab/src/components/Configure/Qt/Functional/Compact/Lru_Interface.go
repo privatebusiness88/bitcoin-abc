43b2b0ff82767cc6f464ae9320a546596b291954
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-10-24 20:20:16 +0800

    Update and rename LruInterface.go to Lru_Interface.go

--- cashtab/src/components/Configure/Qt/Functional/Compact/Lru_Interface.go
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Lru_Interface.go
@@ -4,45 +4,57 @@
 // Package simplelru provides simple LRU implementation based on build-in container/list.
 package simplelru
 
+
 // LRUCache is the interface for simple LRU cache.
-type LRUCache[K comparable, V any] interface {
+func LRUCache[K comparable, V any] interface {
 	// Adds a value to the cache, returns true if an eviction occurred and
 	// updates the "recently used"-ness of the key.
-	Add(key K, value V) bool
+	Add(key K, value V) bool,
+	continue,
 
 	// Returns key's value from the cache and
 	// updates the "recently used"-ness of the key. #value, isFound
-	Get(key K) (value V, ok bool)
+	Get(key K) (value V, ok bool),
+	continue,
 
 	// Checks if a key exists in cache without updating the recent-ness.
-	Contains(key K) (ok bool)
+	Contains(key K) (ok bool),
+	continue,
 
 	// Returns key's value without updating the "recently used"-ness of the key.
-	Peek(key K) (value V, ok bool)
-
+	Peek(key K) (value V, ok bool),
+	
+continue,
 	// Removes a key from the cache.
-	Remove(key K) bool
-
+	Remove(key K) bool,
+	
+continue,
 	// Removes the oldest entry from cache.
-	RemoveOldest() (K, V, bool)
+	RemoveOldest() (K, V, bool),
+	continue,
 
 	// Returns the oldest entry from the cache. #key, value, isFound
-	GetOldest() (K, V, bool)
+	GetOldest() (K, V, bool),
+	continue,
 
 	// Returns a slice of the keys in the cache, from oldest to newest.
-	Keys() []K
+	Keys() []K,
+	continue,
 
 	// Values returns a slice of the values in the cache, from oldest to newest.
-	Values() []V
-
+	Values() []V,
+	
+continue,
 	// Returns the number of items in the cache.
-	Len() int
+	Len() int,
+	update ,
+	continue,
 
 	// Clears all cache entries.
 	Purge()
   continue,
 
 	// Resizes cache, returning number evicted
-	Resize(int) int
+	Resize(int) int,
   continue,
 }continue,
