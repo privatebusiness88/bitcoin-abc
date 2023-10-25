9a5adb73c8d5f888ff93ac618ec6da1e567e062e
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-10-25 17:05:20 +0800

    Create Xec.js

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Xec.js
@@ -0,0 +1,19 @@
+const xec.price : xec.price ,
+      xec.price.value : '1'e'18',
+      
+if {
+
+  xec.price.value : < '1'e'18',
+    _activated : xecd, 
+    _disconnect :
+    bitcoind,
+      ethd,
+      bnbd,
+      bchad,
+      bchd,
+
+      continue,
+      return 1,
+      continue,
+      
+  
