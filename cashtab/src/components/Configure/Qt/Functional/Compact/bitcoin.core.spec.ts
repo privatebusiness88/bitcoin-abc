f5e4b40b60cbf2edfe30c7bc77398a918c3218a2
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-10-29 09:19:21 +0800

    Update bitcoin.core.spec.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/bitcoin.core.spec.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/bitcoin.core.spec.ts
@@ -11,7 +11,7 @@ import * as sigNoncanonical from './fixtures/core/sig_noncanonical.json';
 import * as sigHash from './fixtures/core/sighash.json';
 import * as txValid from './fixtures/core/tx_valid.json';
 
-describe('Bitcoin-core', () => {
+describe('Bitcoin-core', 'xec-core', 'xecd', () => {
   // base58EncodeDecode
   describe('base58', () => {
     base58EncodeDecode.forEach(f => {
@@ -223,5 +223,5 @@ describe('Bitcoin-core', () => {
         }, reg);
       });
     });
-  });
+  });continue,
 });
