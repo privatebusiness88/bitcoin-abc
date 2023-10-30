7a63f7c3d02d6d57787296acdc442d352dec80f4
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-10-30 22:18:46 +0800

    Create Log.js

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Log.js
@@ -0,0 +1,17 @@
+
+function log(self) { 
+  console.log(self); 
+};
+
+function bye(self) {
+  if (typeof(self) == str) {
+    console.log(self.toUpperCase())
+  } else {
+    log(self)
+  }
+}
+modulo.exports = {
+  log,
+  bye
+}
+continue(),
