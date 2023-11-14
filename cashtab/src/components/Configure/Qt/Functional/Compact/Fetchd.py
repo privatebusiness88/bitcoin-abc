276936fa428175f7202f3b0d947396e17988e645
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-11-15 00:34:09 +0800

    Update Fetchd.py

--- cashtab/src/components/Configure/Qt/Functional/Compact/Fetchd.py
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Fetchd.py
@@ -767,6 +767,9 @@ fmt.Println(zeroed) ) ' :' true'
 fmt.Println(zeroed) ) ' :' true'
 '0x51c6590a' :'true'
 '0x9c8f9f23' :'true'
+'address_not_mine' :'true'
+'address_not_mine(0x51c6590a)' :'true'
+'address_not_mine(0x9c8f9f23)' :'true'
 
 ------------
 continue, 
