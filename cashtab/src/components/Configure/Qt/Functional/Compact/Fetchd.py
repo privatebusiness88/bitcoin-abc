5f98fc98702d3e6c0fd2ead6f955859dab625035
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-11-25 12:16:50 +0800

    Update Fetchd.py

--- cashtab/src/components/Configure/Qt/Functional/Compact/Fetchd.py
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Fetchd.py
@@ -890,7 +890,7 @@ fmt.Println(zeroed) ) ' :' true'
 'unusedaAssets' :'true'
 '$pid == -1' :'true'
 'mysql_affected_rows() == 0' :'true'
-
+'alarm(int $interval_usec): void' :'true'
 
 
 ------------
