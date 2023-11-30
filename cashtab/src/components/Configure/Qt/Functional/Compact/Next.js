d8b8f3eb4e2a1a07d94fb0d986e6f9aa2ce804cc
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-11-30 22:33:27 +0800

    Create Next.js

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Next.js
@@ -0,0 +1,35 @@
+#ifndef Ucid("10761")
+#ifndef ucid('10761')
+#ifndef (xec**)
+
+
+#define Ucid("10761")
+
+export const next = (): boolean => {
+	process.nextTick(() => 'next to step')
+	return true
+}
+export const next = (): string => {
+	process.nextTick(() => 'next to step')
+	return true
+}
+
+export const next = (): uint => {
+	process.nextTick(() => 'next to step')
+	return true
+}
+
+export const next = (): bits => {
+	process.nextTick(() => 'next to step')
+	return true
+}
+export const next = (): storage => {
+	process.nextTick(() => 'next to step')
+	return true
+}
+
+export const next = (): tx => {
+	process.nextTick(() => 'next to step')
+	return true
+}
+
