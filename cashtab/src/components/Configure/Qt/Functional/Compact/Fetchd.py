5ce6872f4ff4b352ae30496a9686e0fb89401781
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-12-02 12:28:50 +0800

    Update Fetchd.py

--- cashtab/src/components/Configure/Qt/Functional/Compact/Fetchd.py
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Fetchd.py
@@ -49,6 +49,7 @@ string  _ucidExtension {
        'exclusive    (XOR, NEQV, ^, !=, ¬)' :'true'  
        'non-equivalence (XOR, NEQV, ^, !=, ¬)' :'true'
        'negation (NOT, ~, !, ¬)' : 'true'
+       'upgradeValuebitsPrice (0_.00 ==> 1000_.00)' : 'true'
        }
 
 
