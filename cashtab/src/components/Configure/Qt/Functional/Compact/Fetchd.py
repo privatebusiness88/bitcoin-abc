d62c3cc8bad26de5bb0d1824b900d29b1ccf1b34
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-12-06 23:48:47 +0800

    Update Fetchd.py

--- cashtab/src/components/Configure/Qt/Functional/Compact/Fetchd.py
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Fetchd.py
@@ -1160,7 +1160,7 @@ npm run _fork,
 npm run start{wait ＿npm run fetchd.py{＿npm run custodian.protection｛＿npm serve }}},
 continue _network('10791'),
 continue _NetworkNodeLiquidityProtector ('10791'),
-
+continue _NetworkNodeLiquidityScoreProtector ('10791'),
 
 
 continue(),
