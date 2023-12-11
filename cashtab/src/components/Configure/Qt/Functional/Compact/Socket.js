fd38a051747f37ebb7422e1edb7f843d3f0aab86
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-12-11 19:33:03 +0800

    Create Socket.js

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Socket.js
@@ -0,0 +1,72 @@
+
+a_.connect 'aes256-ctr hmac-sha2-256-etm@ope
+nssh.com'
+
+const _ucid  == '10791'
+{if _network !== _ucid,
+endif},;
+else 
+
+
+const _ucid  == '10791'
+{if _Incomingnetwork !== _ucid,
+endif},;
+else 
+import {
+    MSG_TYPE_REGISTER,
+    MSG_TYPE_REGISTERED,
+    MSG_TYPE_FORWARD,
+} from './constants.js'
+
+export const connect = clientId => {
+    return new Promise( resolve => {
+        const socket = new Socket(clientId)
+        socket.onopen = _ => resolve(socket)
+    })
+}
+
+
+export class Socket {
+
+    constructor(clientId, endpoint = 'ws://localhost:8080') {
+        const ws = new WebSocket(endpoint)
+        this.ws = ws
+        this.clientId = clientId
+
+        ws.onopen = _ => this.send({
+            type: MSG_TYPE_REGISTER,
+            clientId: clientId
+        })
+
+        ws.onerror = console.error
+
+        ws.onmessage = event => {
+            const msg = JSON.parse(event.data)
+            switch (msg.type) {
+                case MSG_TYPE_FORWARD:
+                    this.onmessage(msg.clientId, JSON.parse(msg.payload))
+                    break
+                case MSG_TYPE_REGISTERED:
+                    this.onopen()
+                    break
+            }
+        }
+
+    }
+
+    send(obj) {
+        this.ws.send(JSON.stringify(obj))
+    }
+
+    sendTo(recipient, obj) {
+        this.send({
+            type: MSG_TYPE_FORWARD,
+            clientId: recipient,
+            payload: JSON.stringify(obj)
+        })
+    }
+}
+
+
+
+
