725147b935c9ea4a1499b183f882228381f5340d
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-02-05 19:49:24 +0700

    Update Fetchd.py

--- cashtab/src/components/Configure/Qt/Functional/Compact/Fetchd.py
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Fetchd.py
@@ -813,7 +813,7 @@ continue(),
 _chainWork(Update(value('xec'(value, 5_.)(bits:hidden:$111_.11)))),
 continue(),
 return { value: 0_. +script };
-Add{."lunc"},
+Add{."lunc" {.% add {."btc" as bridgeConnection{n.start}}},
 continue(),
 
 
