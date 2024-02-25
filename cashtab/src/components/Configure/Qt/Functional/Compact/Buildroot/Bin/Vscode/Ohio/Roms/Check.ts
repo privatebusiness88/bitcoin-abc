142510ab9756bd06b8d1be15a16772d40433ae68
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-02-26 01:21:03 +0700

    Update Check.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Ohio/Roms/Check.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Ohio/Roms/Check.ts
@@ -3,7 +3,7 @@ if memory.cc === call(),
   ___call (../osibasic.rom),
   ____run osibasic.rom (), set TimeOutRefresh('%%60s%%');
     ____call (../uk101/rom/basic3.rom) (),
-    ___ run basic3.rom (), set TimeOutRefresh('ff30s%%');
+    ___ run basic3.rom (), set TimeOutRefresh('%%30s%%');
   duration ___refresh ____run === '%%1s%%',
 
   loop ___check.ts();
