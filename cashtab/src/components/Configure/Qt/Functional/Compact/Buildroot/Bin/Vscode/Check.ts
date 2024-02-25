0b684ff362c701110b93d0f39cdb88def95d2e5f
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-02-26 01:12:48 +0700

    Update Check.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Check.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Check.ts
@@ -1,5 +1,7 @@
 if memory.cc === call(), 
   ____run memory.h (),
+  ___call (../ohio/rom/osibasic.rom),
+  ____run osibasic.rom (),
   duration ___refresh ____run === '%%1s%%',
 
   loop ___check.ts();
