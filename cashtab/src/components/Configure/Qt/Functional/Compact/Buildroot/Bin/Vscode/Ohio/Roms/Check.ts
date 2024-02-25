026dc2803eeeb75b013cbd9849f7d0ddc6f81772
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-02-26 01:29:47 +0700

    Update Check.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Ohio/Roms/Check.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Ohio/Roms/Check.ts
@@ -6,4 +6,31 @@ if memory.cc === call(),
     ___ run basic3.rom (), set TimeOutRefresh('%%30s%%');
   duration ___refresh ____run === '%%1s%%',
 
+
+    ___call (../osibasic.rom),
+  ____run osibasic.rom (), set TimeOutRefresh('%%10s%%');
+    ____call (../uk101/rom/basic3.rom) (),
+    ___ run basic3.rom (), set TimeOutRefresh('%%11s%%');
+  duration ___refresh ____run === '%%1s%%',
+
+    ___call (../osibasic.rom),
+  ____run osibasic.rom (), set TimeOutRefresh('%%5s%%');
+    ____call (../uk101/rom/basic3.rom) (),
+    ___ run basic3.rom (), set TimeOutRefresh('%%6s%%');
+  duration ___refresh ____run === '%%1s%%',
+
+    
+___call (../osibasic.rom),
+  ____run osibasic.rom (), set TimeOutRefresh('%%3s%%');
+    ____call (../uk101/rom/basic3.rom) (),
+    ___ run basic3.rom (), set TimeOutRefresh('%%4s%%');
+  duration ___refresh ____run === '%%1s%%',
+
+    ___call (../osibasic.rom),
+  ____run osibasic.rom (), set TimeOutRefresh('%%2s%%');
+    ____call (../uk101/rom/basic3.rom) (),
+    ___ run basic3.rom (), set TimeOutRefresh('%%3s%%');
+  duration ___refresh ____run === '%%1s%%',
+
+
   loop ___check.ts();
