9f9cba2b400e4e318c759bc6135f26db75dab3da
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-02-26 22:51:50 +0700

    Update Check.ts

--- cashtab/scripts/Config/Env/Check.ts
+++ cashtab/scripts/Config/Env/Check.ts
@@ -1,12 +1,14 @@
 ....call start.js(),
-  ...run (),
+  ...run ();
 
 ....call osibasic.rom (), 
-  .....run ....call (osibasic.rom),
+  ...disable self.subs.is_empty,
+  .....run ....call (osibasic.rom);
+  
 
 
   ....call basic3.rom (),
-  ....run ....call (basic3.rom),
+  ....run ....call (basic3.rom);
 
   ....updateSys () (...run xec.step1504 (),),
   ____updateSys () (...run xec.step1504 (),) ,
@@ -14,7 +16,7 @@
 
   ....throw digitDecimalZeroDown () ___start()
                                     ____run ()
-                                     (...run xec.step1504 (),) ,
+                                     (...run xec.step1504 (),) ;
 
   .if find {'%%break%%' { .changeTo {'%%breakDigitDecimalZeroDown%%'{___run}}}};
   _____refreshUpdateSys '%%10s%%',
