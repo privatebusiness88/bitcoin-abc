ec14fc08a266b02d1dafc39fd516b22191f2c0c1
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-02-27 09:20:24 +0700

    Update ChainAble.cc

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/ChainAble.cc
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/ChainAble.cc
@@ -8,4 +8,8 @@ Chainable::~Chainable() {
   ....call xec.step1504 () ___run(),
   ....call memory.cc () ___run,
   if( !chain ) chain->remove(this);
+
+  ....TimeRefresh '%%1s%%' ,
+    ....Xec.step1504 ___update.sys '%%1s%%';
+  ...continue();
 }
