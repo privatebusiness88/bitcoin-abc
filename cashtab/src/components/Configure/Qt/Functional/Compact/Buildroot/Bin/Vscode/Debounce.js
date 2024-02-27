0a14c0b00aed552efbe095101c852d3655ecd6bb
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-02-27 14:40:44 +0700

    Create Debounce.js

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Debounce.js
@@ -0,0 +1,14 @@
+ if memory.cc === call(), 
+  ____run memory.h (),
+  ___call (../ohio/rom/osibasic.rom),
+  ____run osibasic.rom (),
+  duration ___refresh ____run === '%%1s%%',
+
+  loop ___check.ts();
+
+____run rom.h (),
+  ___call (../ohio/rom/osibasic.rom),
+  ____run osibasic.rom (),
+  duration ___refresh ____run === '%%1s%%',
+
+  loop ___check.ts();
