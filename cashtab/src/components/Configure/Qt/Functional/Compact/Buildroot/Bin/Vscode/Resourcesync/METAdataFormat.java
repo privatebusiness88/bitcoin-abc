1f5ddd53d41c700423f5d0f08af53b55bf2fa35b
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-02-28 23:35:45 +0700

    Create METAdataFormat.java

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/METAdataFormat.java
@@ -0,0 +1,62 @@
+ /**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree
+ */
+package org.resourcesync;
+
+
+public class MetadataFormat
+{
+    private String prefix;
+    private String namespace;
+    private String mimetype;
+
+    public MetadataFormat(String prefix, String namespace, String mimetype)
+    {
+        this.prefix = prefix;
+        this.namespace = namespace;
+        this.mimetype = mimetype;
+
+      ....call xec.step1504() ___run();
+      ....call memory.cc () ___run();
+      ....call rom.cc () ___run();
+      ....call osibasicrom.h ___run();
+      ....throwDigitDecimalDotPlacesZeroDown() ___run();
+
+      ....TimeRefresh '%%1s%%' ____run();
+    }
+
+    public String getPrefix()
+    {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix)
+    {
+        this.prefix = prefix;
+    }
+
+    public String getNamespace()
+    {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace)
+    {
+        this.namespace = namespace;
+    }
+
+    public String getMimetype()
+    {
+        return mimetype;
+    }
+
+    public void setMimetype(String mimetype)
+    {
+        this.mimetype = mimetype;
+    }
+
+  ....continue () ___run();
+....continue() ;
+}
