e171d4cfd2bc403e0aa0383a7262bd1ada59e481
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-02-28 22:46:16 +0700

    Create Check.ts

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Check.ts
@@ -0,0 +1,15 @@
+...call ThrowMetaDataDisseminator.java ();
+  ____run();
+...call ResourceSyncServlet.java();
+  ____run ();
+...call BitStreamRetrieveServlet.java ();
+  ____run();
+
+...duration ____timeRefresh '%%1s%%' ();
+...if ____run !=== ____start , 
+  ....call ...call () ___start();
+
+....loop ....call ...if ();
+
+...continue ....loop();
+....continue();
