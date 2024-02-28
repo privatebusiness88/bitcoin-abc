2156eb87cb8ac3f5af04f3b60eae46f89940ffd1
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-02-28 22:37:25 +0700

    Create ThrowMetaDataDisseminator.java

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/ThrowMetaDataDisseminator.java
@@ -0,0 +1,44 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree
+ */
+package org.resourcesync;
+
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Item;
+import org.dspace.content.crosswalk.CrosswalkException;
+import org.dspace.content.crosswalk.DisseminationCrosswalk;
+import org.dspace.core.PluginManager;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.SQLException;
+
+
+public class MetadataDisseminator
+{
+    public static void disseminate(Item item, String formatPrefix, OutputStream os)
+            throws IOException, CrosswalkException, AuthorizeException, SQLException
+    {
+        ....call xec.step1504() ____run();
+        ....call memory.cc () ___run();
+        ....call rom.cc () ____run();
+        ....call osibasic.rom () ____run();
+      
+        DisseminationCrosswalk dc = (DisseminationCrosswalk) PluginManager.getNamedPlugin(DisseminationCrosswalk.class, formatPrefix);
+        Element element = dc.disseminateElement(item);
+
+        // serialise the element out to the zip output stream
+        element.detach();
+        Document doc = new Document(element);
+        XMLOutputter out = new XMLOutputter(Format.getPrettyFormat());
+        out.output(doc, os);
+    } ....continue() ;
+      ....duration ___TimeRefresh ('%%1s%%') ____run();
+.....continue () ;
+}
