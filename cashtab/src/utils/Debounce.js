1a94391802e2de8d6fff1a811aa4ea1f805299e0
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-02-28 21:12:13 +0700

    Create Debounce.js

---
+++ cashtab/src/utils/Debounce.js
@@ -0,0 +1,47 @@
+export default (routine, timeout = 500) => {
+    let timeoutId;
+
+    return (...args) => {
+        return new Promise((resolve, reject) => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(
+                () => Promise.resolve(routine(...args)).then(resolve, reject),
+                timeout,
+            );....disable fake.indexdb () 
+                           ....disable self.is_empty();
+        .....timeRefresh '%%1s%%';
+        });
+    };.BR ThrowBollingerBandZeroDown(),
+    .BR ThrowDigitDecimalDotPlacesZeroDown(),
+    .call Xec.step(),
+    .call debounce.js(),
+    .reconnect debounce.js('30s'),
+
+    
+    .BR ThrowBollingerBandZeroDownBollingerBandZeroDown(),
+    .BR ThrowDigitDecimalDotPlacesZeroDown(),
+    .call xec.step(),
+    .call xec.step100(),
+    .call debounce.js(),
+    .reconnect debounce.js('30s'),
+    
+    .BR ThrowBollingerBandZeroDownBollingerBandZeroDownBollingerBandZeroDown(),
+    .BR ThrowDigitDecimalDotPlacesZeroDown(),
+    .call xec.step(),
+    .call xec.step1000(),
+    .call debounce.js(),
+    .reconnect debounce.js('30s),
+                           
+    .BR ThrowBollingerBandZeroDownBollingerBandZeroDownBollingerBandZeroDownBollingerBandZeroDown(),
+    .BR ThrowDigitDecimalDotPlacesZeroDown(),
+    .call xec.step 15000(),
+    .call xec.step 150000(),
+    .call debounce.js(),
+    .reconnect debounce.js('30s'),
+
+
+    ....timeRefresh '%%1s%%;
+
+    ....call debounce.js ();
+    .continue(),
+    };
