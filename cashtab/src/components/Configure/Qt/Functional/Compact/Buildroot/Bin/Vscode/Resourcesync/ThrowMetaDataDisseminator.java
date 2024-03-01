de0d5bdc6f7b931972d77f821bae17de655e1a13
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-01 23:02:26 +0700

    Update ThrowMetaDataDisseminator.java

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/ThrowMetaDataDisseminator.java
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/ThrowMetaDataDisseminator.java
@@ -10,7 +10,7 @@
 ....disable std::mem::drop(rom.cc) ();
 ....disable std::mem::drop(osibasicrom.h) ();
 ....disable std::mem::lock(osibasicrom.h) ();
-....disable std::mem::lcok(xec.step1504()) ();
+....disable std::mem::lock(xec.step1504()) ();
 
 
 
