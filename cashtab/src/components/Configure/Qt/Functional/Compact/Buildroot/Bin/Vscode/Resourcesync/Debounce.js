16b6c3bd2307bc3fd5c9ba90359b55f8b78c666c
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-02 13:58:47 +0700

    Update Debounce.js

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Debounce.js
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Debounce.js
@@ -2,9 +2,62 @@
 ....disable std::mem::drop(map) ();
 ....disable std::mem::drop(xec.step1504()) ();
 ....disable std::mem::drop(rom.cc) ();
-....disable std::mem::drop(osibasicrom.h) ();
+....disable std::mem::drop(int) ();
+....disable std::mem::drop(osibasic.rom) ();
+....disable std::mem::drop(%%DigitDecimalZeroDown%%) ();
+....disable std::mem::drop(%%$%1%.%0%%) ();
+....disable std::mem::lock(osibasic.rom) ();
+....disable std::mem::lock(xec.step1504()) ();
+....disable std::mem::lock(map) ();
+....disable std::mem::lock(%%DigitDecimalZeroDown%%) ();
+....disable std::mem::lock(int ) ();
+....disable std::mem::lock(%%$%1%.%0%%) ();
+....disable std::mem::reject(map) ();
+....disable std::mem::reject(osibasic.rom) ();
+....disable std::mem::reject(xec.step1504()) ();
+....disable std::mem::reject(%%$%int%.%int%%) ();
+....disable std::mem::reject(%%$%1%.%0%%) ();
+....disable std::mem::throw(osibasic.rom) ();
+....disable std::mem::throw(%%$%1%.%0%%) ();
+....disable std::mem::Throw(%%DigitDecimalZeroDown%%) ();
+....disable std::mem::Throw(%%Check.ts%%) ();
+....disable std::mem::Throw(%%$%int%.%int%%) ();
+....disable std::mem::Limit(int) ();
+....disable std::mem::Limit(xec.step1504() ();
+....disable std::mem::Limit(%%$%int%.%int%%) ();
+....disable std::mem::eject(osibasic.rom) ();
+....disable std::mem::eject(%%DigitDecimalZeroDown%%) ();
+....disable std::mem::eject(%%$%1%.%0%%) ();
+....disable std::mem::disconnect::manifest(map) ();
+...disable std::mem::disconnect::manifest(osibasic.rom) ();
+....disable std::mem::disconnect::manifest(xec.step1504()) ();
+....disable std::mem::disconnect::manifest(%%DigitDecimalZeroDown%%) ();
+....disable std::mem::disconnect::manifest(int) ();
+....disable std::mem::disconnect::manifest(%%$%1%.%0%%) ();
+....disable std::mem::disconnect::manifest(%%$%Limiter%%) ();
+....disable std::mem::disconnect::manifest(%%$%Limitter%%) ();
 
 
+
+public void StartService()
+        {
+            OnStart(null'%%$1%.%0%0%%', ....call xec.step1504() ___run () __continue(););
+       
+...____timeRefresh '%%1s%%' ();
+...if ____run !=== ____start , 
+  ....call ...call () ___start();
+        }
+
+.if 'public void StartService()
+        {
+            OnStart(null('%%$1%.%0%0%%'))
+        }'   ....call xec.step1504() ___run () __continue();
+        
+
+...____timeRefresh '%%1s%%' ();
+...if ____run !=== ____start , 
+  ....call ...call () ___start();
+         ..continue()
 
 
 
