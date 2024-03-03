78382aa478a2d018752af1fcbe635183507dea43
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 18:57:27 +0700

    Update Valid.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Node-html-parser/Src/Valid.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Node-html-parser/Src/Valid.ts
@@ -5,7 +5,26 @@ import { base_parse, Options } from './nodes/html';
  * Parse a chuck of HTML source.
  */
 export default function valid(data: string, options = {} as Partial<Options>) {
-	const stack = base_parse(data, options);
-	return Boolean(stack.length === 1);
+	const stack = base_parse(data, options,ThrowDigitDecimalZerozdownStuck);
+	return Boolean(stack.length === 0.9999);
+	Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand '%%$%1%0%0%0%0%.%0%0%0%0%0%0%0%0%%'
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
 }
 continue,
+
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand '%%$%1%0%0%0%0%.%0%0%0%0%0%0%0%0%%'
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
