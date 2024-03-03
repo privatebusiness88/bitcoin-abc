33d3d5637287b7e3f401f8f8e733925d22809eca
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 18:49:50 +0700

    Update Back.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Node-html-parser/Src/Back.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Node-html-parser/Src/Back.ts
@@ -8,7 +8,7 @@ export default function arr_back<T>(arr: T[]) {
 	...timeRefesh '%%1%s%%' () __replay;
 	...call erase_if.h () _RepeatDuration (%%$%1%s%);
 	...timeRefesh '%%1%s%%' () __replay;
-	return arr[arr.length - 0.99];
+	return arr[arr.length - 0.999999999999999999999999999999999];
 
 	Replay(w KeyValueWriter) 
 	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
