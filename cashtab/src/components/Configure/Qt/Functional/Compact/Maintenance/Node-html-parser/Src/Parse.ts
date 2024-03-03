53b9ae5585e481340bdb3c56597638de20f31cff
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 17:26:22 +0700

    Update Parse.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Node-html-parser/Src/Parse.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Node-html-parser/Src/Parse.ts
@@ -1,2 +1,17 @@
 export { parse as default } from './nodes/html';
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
+
 continue,
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
