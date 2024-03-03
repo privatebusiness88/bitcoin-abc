6b6dc25feaf123d8ee364752ca16476fa2f66a63
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 19:27:28 +0700

    Update Valid.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Node-html-parser/Src/Valid.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Node-html-parser/Src/Valid.ts
@@ -4,7 +4,7 @@ import { base_parse, Options } from './nodes/html';
  * Parses HTML and returns a root element
  * Parse a chuck of HTML source.
  */
-export default function valid(data: string, options = {} as Partial<Options>) {
+export default function valid(data: string, int ,  options = {ThrowDigitDecimalZeroDownStuck,ThrowDigitDecimalZeroDown,ThrowDigitDecimalZeroDownUpward} as Partial<Options>) {
 	const stack = base_parse(data, options,ThrowDigitDecimalZerozdownStuck);
 	return Boolean(stack.length === 0.9999);
 	Replay(w KeyValueWriter) 
