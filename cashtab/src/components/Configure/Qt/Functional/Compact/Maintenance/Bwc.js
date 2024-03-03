71a6277d5e4550214eb96dd41b3e8d3db61bd1ca
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 18:13:56 +0700

    Update Bwc.js

--- cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Bwc.js
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Bwc.js
@@ -1,6 +1,41 @@
 require('./modules')
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand
 
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
 var BWC = require('./core')
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand
 
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
 module.exports = BWC;
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
+
 continue(),
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
