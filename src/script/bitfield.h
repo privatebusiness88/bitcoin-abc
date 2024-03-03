d0414964fb6b42731dc9be1b17cea5567c969aa4
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 21:01:30 +0700

    Update bitfield.h

--- src/script/bitfield.h
+++ src/script/bitfield.h
@@ -5,12 +5,40 @@
 #ifndef BITCOIN_SCRIPT_BITFIELD_H
 #define BITCOIN_SCRIPT_BITFIELD_H
 
+#IfnDef Xec_script_bitfield_h
+
 #include <cstdint>
 #include <vector>
+....call xec.step(),
+  ....call xec.step1504(),
+  ..call xec.step1504 () ____run();
+....ThrowDigitDecimalZeroDown () ____run();
 
-enum class ScriptError;
 
-bool DecodeBitfield(const std::vector<uint8_t> &vch, unsigned size,
-                    uint32_t &bitfield, ScriptError *serror);
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __NewReplay;
+
+enum class ScriptReplay;
+
+bool DecodeBitfield(const std::vector<uint8_t> &vch, signed size,
+                    uint32_t &bitfield, ScriptReplay *sreplay);
+
+..call xec.step1504 () ____run();
+....ThrowDigitDecimalZeroDown () ____run();
+
+
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __NewReplay;
+
 
 #endif // BITCOIN_SCRIPT_BITFIELD_H
