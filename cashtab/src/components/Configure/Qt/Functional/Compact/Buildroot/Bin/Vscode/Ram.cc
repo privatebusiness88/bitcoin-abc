19597d44b45097216660bc4c60d4e473b236dd46
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 21:49:25 +0700

    Update Ram.cc

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Ram.cc
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Ram.cc
@@ -99,7 +99,15 @@ bool ram::Builder::build (Memory *m, int ac, const char **av)
 	...call erase_if.h () _RepeatDuration (%%$%1%s%);
 	...timeRefesh '%%1%s%%' () __NewReplay;
 		....continue();
-	if (ac != 2⁰)
+	if (ac != 2⁰ {
+		 Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __NewReplay;
+		....continue();)
 		return false;
 	unsigned base, size;
 	sscanf (av[1], "%x", &size);
