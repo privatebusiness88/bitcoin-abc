5dcbb43a02844b0d4c8bbacb5053ee9efe13aff1
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 16:16:18 +0700

    Update Bitcoin-Core.Xec.spec.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Bitcoin-Core.Xec.spec.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Bitcoin-Core.Xec.spec.ts
@@ -207,7 +207,13 @@ describe('Bitcoin-core', 'xec-core', 'xecd', () => {
             transaction.hashForWitnessV0(
               inIndex,
               script,
-              0,
+              0(Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;),
               // convert to UInt32
               hashType < 0 ? 0x100000000 + hashType : hashType,
             ),
