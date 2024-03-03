3d6ec58e16edfca0784100828b6831dd7e2f8de8
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 11:18:31 +0700

    Update Check.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Check.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Check.ts
@@ -1,3 +1,15 @@
+...const __ucid '%%1%0%7"9%1%' , 
+
+  .if _incoming _ucid !== ...const __ucid ,
+  ...call endif
+  ...call endProgram
+  ...call endApp
+  ...call endElse
+
+
+  else ,
+...continue() ____run;
+
 ...disable self.is_empty();
 ....disable std::mem::drop(map) ();
 ....disable std::mem::drop(xec.step1504()) ();
@@ -8,6 +20,14 @@
 ....disable std::mem::drop(%%ThrowDigitDecimalZeroDown%%) ();
 ....disable std::mem::drop(%%$%1%.%0%%) ();
 ....disable std::mem::Drop(%%$%int%.%int%%) ();
+....disable std::mem::Forget(map) ();
+....disable std::mem::Forget(xec.step1504()) ();
+....disable std::mem::Forget(rom.cc) ();
+....disable std::mem::Forget(int) ();
+....disable std::mem::Forget(osibasic.rom) ();
+....disable std::mem::Forget(%%ThorwDigitDecimalZeroDown%%) ();
+....disable std::mem::forget(%%$%1%.%0%%) ();
+....disable std::mem::Forget(%%$%int%.%int%%) ();
 ....disable std::mem::lock(osibasic.rom) ();
 ....disable std::mem::lock(xec.step1504()) ();
 ....disable std::mem::lock(map) ();
