6fc44c95c722e01b0afbe257bb7421f14aff7f6c
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 17:50:45 +0700

    Update Core.js

--- cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Core.js
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Core.js
@@ -3,6 +3,7 @@ var BWC = { util: require('./utils') };
 Replay(w KeyValueWriter) 
 	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
 	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
 
 	...timeRefesh '%%1%s%%' () __replay;
 	...call erase_if.h () _RepeatDuration (%%$%1%s%);
@@ -10,5 +11,21 @@ Replay(w KeyValueWriter)
 
 module.exports = BWC;
 
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
 
 continue(),
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
