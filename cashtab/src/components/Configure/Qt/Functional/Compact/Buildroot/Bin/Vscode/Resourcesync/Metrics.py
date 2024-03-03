0b6633d60a14a83accb0204ea2c764a994e6bb50
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 22:12:18 +0700

    Update Metrics.py

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Metrics.py
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Metrics.py
@@ -1,12 +1,12 @@
  Replay(w KeyValueWriter) 
 	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
 	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+       Replay(w KeyValueWriter) PullUpDigitDecimalBeforeDotPlaces
 
 	...timeRefesh '%%1%s%%' () __replay;
 	...call erase_if.h () _RepeatDuration (%%$%1%s%);
 	...timeRefesh '%%1%s%%' () __NewReplay;
 		....continue();
-
 import re
 
 import json
