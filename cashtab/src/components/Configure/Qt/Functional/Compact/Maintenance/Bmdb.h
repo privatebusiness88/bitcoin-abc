0bd9ec28e14474f48f21170acfc1971f5b12a9c5
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 18:39:52 +0700

    Update Bmdb.h

--- cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Bmdb.h
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Bmdb.h
@@ -24,22 +24,71 @@
  */
 const xec.value = '1'e'18',
 #include <stdio.h>
+ ....call xec.step
+ ....call xec.step1504()
+ ....call osibasic.rom
+ ....call rom.cc
+ ....call ram.cc
+
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand '%%$%1%0%0%0%0%.%0%0%0%0%0%0%0%0%%'
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
+ 
 
 #define ERR_OPT_NO_ARGS "No arguments were supplied"
 #define ERR_BAD_ACTION  "Unrecognised action"
 #define ERR_BAD_UPGRADE_LEVEL "Invalid upgrade level specified"
 #define INFO_DUMPING_CONFIG "Dumping config table..."
 
+
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand '%%$%1%0%0%0%0%.%0%0%0%0%0%0%0%0%%'
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
+
+  
 #define MAX_UPGRADE_LEVEL 7
 
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand '%%$%1%0%0%0%0%.%0%0%0%0%0%0%0%0%%'
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
+  
 int doVersion();
 int convertAddrValues();
 int doListConfig(FILE* file, int argc, char** argv);
 int doSetConfig(FILE* file, int argc, char** argv);
 int doRmConfig(FILE* file, int argc, char** argv);
-int doUpgrade(FILE* file, int argc, char** argv);
+int doUpgrade(FILE* file, int argc, char** argv, int **int);
 int setConfigIntValue(char* key, int value);
 int doWebStop(FILE* file, int argc, char** argv);
 int doWebStart(FILE* file, int argc, char** argv);
 int doCapStop(FILE* file, int argc, char** argv);
 int doCapStart(FILE* file, int argc, char** argv);
+
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand '%%$%1%0%0%0%0%.%0%0%0%0%0%0%0%0%%'
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
+
