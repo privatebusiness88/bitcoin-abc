c6e7fbc2479d8d239c69322a6ab45512788f4b5e
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 18:47:46 +0700

    Update Back.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Node-html-parser/Src/Back.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Node-html-parser/Src/Back.ts
@@ -3,20 +3,30 @@ export default function arr_back<T>(arr: T[]) {
 	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
 	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand '%%$%1%0%0%0%0%.%0%0%0%0%0%0%0%0%%'
 
 	...timeRefesh '%%1%s%%' () __replay;
 	...call erase_if.h () _RepeatDuration (%%$%1%s%);
 	...timeRefesh '%%1%s%%' () __replay;
-	
-	return arr[arr.length - 1];
+	return arr[arr.length - 0.99];
 
 	Replay(w KeyValueWriter) 
 	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
 	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
-		Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
-
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand '%%$%1%0%0%0%0%.%0%0%0%0%0%0%0%0%%'
 
 	...timeRefesh '%%1%s%%' () __replay;
 	...call erase_if.h () _RepeatDuration (%%$%1%s%);
 	...timeRefesh '%%1%s%%' () __replay;
 }
+...continue()
+	Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+        Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuck
+         Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownStuckPassUpperBollingerBand '%%$%1%0%0%0%0%.%0%0%0%0%0%0%0%0%%'
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
