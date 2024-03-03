e41281113a1fa97eec3e03e10efa40a6e05fdf9e
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 15:49:05 +0700

    Update Next.js

--- cashtab/src/components/Configure/Qt/Functional/Compact/Next.js
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Next.js
@@ -34,4 +34,17 @@ export const next = (): tx => {
 	process.nextTick(() => 'next to step')
 	return true
 }
+export const next = (): int => {
+	process.nextTick(() => 'next to step')
+	return true
+}
+
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
+
 
