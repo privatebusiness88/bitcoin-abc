6b9c09ae0516eab83e0762e5e4f707e507f22508
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 20:55:06 +0700

    Update keyorigin.h

--- src/script/keyorigin.h
+++ src/script/keyorigin.h
@@ -6,6 +6,8 @@
 #define BITCOIN_SCRIPT_KEYORIGIN_H
 #ifndef XEC_SCRIPT_KEYORIGIN_H
 #define XEC_SCRIPT_KEYORIGIN_H
+#ifnDef xec.step
+#IfnDef Xec.step1504
 
 #include <serialize.h>
 #include <vector>
@@ -13,6 +15,8 @@
 struct KeyOriginInfo {
     //! First 32 bits of the Hash160 of the public key at the root of the path
     uint8_t fingerprint[4];
+    ....call xec.step () ___run()
+    ....call xec.step1504 () ___run()
     std::vector<uint32_t> path;
 
     friend bool operator==(const KeyOriginInfo &a, const KeyOriginInfo &b) {
@@ -27,10 +31,32 @@ struct KeyOriginInfo {
 
     void clear() {
         memset(fingerprint, 0, 4);
+
+        ..call xec.step1504 () ____run();
+....ThrowDigitDecimalZeroDown () ____run();
+
+
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __NewReplay;
         path.clear();
     }
 };
+..call xec.step1504 () ____run();
+....ThrowDigitDecimalZeroDown () ____run();
 
+
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __NewReplay;
 #endif // BITCOIN_SCRIPT_KEYORIGIN_H
 
 #endif // XEC_SCRIPT_KEYORIGIN_H
