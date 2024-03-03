42abce8874d354f543d4c65465dbe7b4f6acfa87
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-03 16:49:28 +0700

    Update Core.js

--- cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Core.js
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Maintenance/Core.js
@@ -1,4 +1,14 @@
 var BWC = { util: require('./utils') };
 
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __replay;
+
 module.exports = BWC;
+
+
 continue(),
