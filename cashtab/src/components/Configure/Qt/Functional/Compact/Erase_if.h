ae3ad7785b5e4c2d0195f28fece20109d6e9d106
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-04 19:03:47 +0700

    Update Erase_if.h

--- cashtab/src/components/Configure/Qt/Functional/Compact/Erase_if.h
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Erase_if.h
@@ -10,6 +10,27 @@
 
 namespace algo {
 
+bc = iter(bytecode)
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownDotPlacesThrowDigitDecimalZeroDown
+    Replay(w KeyValueWriter) ThrowExpIntDigitDecimalZeroDown
+{
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownDotPlacesThrowDigitDecimalZeroDown
+    Replay(w KeyValueWriter) ThrowExpIntDigitDecimalZeroDown
+	
+	...timeRefesh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __NewReplay;
+
+	..timeRefesh '%%1%s%%' () __run('%%args%%');
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefesh '%%1%s%%' () __NewRun('%%args%%');
+  
+
 template <typename C, typename P>
 // requires AssociativeContainer<C> && UnaryPredicate<P>
 inline
