2f34414c001a9d71db754b029751e96503f84238
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-05 17:56:56 +0700

    Update Check.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Check.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Check.ts
@@ -11,75 +11,118 @@
   else ,
 ...continue() ____run;
 
+.
 ....disable self.is_empty();
 ....disable std::mem::drop(map) ();
 ....disable std::mem::drop(xec.step1504()) ();
+....disable std::mem::drop(xec.step()) ();
 ....disable std::mem::drop(rom.cc) ();
 ....disable std::mem::drop(int) ();
+....disable std::mem::drop(Ram.cc) ();
+....disable std::mem::drop('%%args%%') ();
 ....disable std::mem::drop(osibasic.rom) ();
 ....disable std::mem::drop(%%DigitDecimalZeroDown%%) ();
-....disable std::mem::drop(%%ThrowDigitDecimalZeroDown%%) ();
+....disable std::mem::Drop(%%DigitDecimalZeroDown%%) ();
 ....disable std::mem::drop(%%$%1%.%0%%) ();
 ....disable std::mem::Drop(%%$%int%.%int%%) ();
-....disable std::mem::Forget(map) ();
-....disable std::mem::Forget(xec.step1504()) ();
-....disable std::mem::Forget(rom.cc) ();
-....disable std::mem::Forget(int) ();
-....disable std::mem::Forget(osibasic.rom) ();
-....disable std::mem::Forget(%%ThorwDigitDecimalZeroDown%%) ();
-....disable std::mem::forget(%%$%1%.%0%%) ();
-....disable std::mem::Forget(%%$%int%.%int%%) ();
 ....disable std::mem::lock(osibasic.rom) ();
 ....disable std::mem::lock(xec.step1504()) ();
 ....disable std::mem::lock(map) ();
+....disable std::mem::Lock(memory.cc) ();
+....disable std::mem::lock(xec.step()) ();
+....disable std::mem::lock(xec.step1504()) ();
 ....disable std::mem::lock(%%DigitDecimalZeroDown%%) ();
-....disable std::mem::lock(%%ThrowDigitDecimalZeroDown%%) ();
+....disable std::mem::lock(%%ExpIntDigitDecimalZeroDown%%) ();
 ....disable std::mem::lock(int ) ();
+....disable std::mem::lock('%%args%%') ();
 ....disable std::mem::lock(%%$%1%.%0%%) ();
 ....disable std::mem::Lock(%%$%int%.%int%%) ();
 ....disable std::mem::reject(map) ();
+....disable std::mem::reject(memory.cc) ();
 ....disable std::mem::reject(osibasic.rom) ();
 ....disable std::mem::reject(xec.step1504()) ();
 ....disable std::mem::reject(%%$%int%.%int%%) ();
+....disable std::mem::Reject(%%DigitDecimalZeroDown%%) ();
+....disable std::mem::Reject(%%ExpIntDigitDecimalZeroDown%%) ();
 ....disable std::mem::reject(%%$%1%.%0%%) ();
+....disable std::mem::reject('%%args%%') ();
 ....disable std::mem::throw(osibasic.rom) ();
 ....disable std::mem::throw(%%$%1%.%0%%) ();
 ....disable std::mem::Throw(%%DigitDecimalZeroDown%%) ();
-....disable std::mem::Throw(%%ThrowDigitDecimalZeroDown%%) ();
+....disable std::mem::Throw(%%DigitDecimalZeroDown%%) ();
 ....disable std::mem::Throw(%%Check.ts%%) ();
 ....disable std::mem::Throw(%%$%int%.%int%%) ();
+....disable std::mem::Throw(memory.cc) ();
+....disable std::mem::Throw(xec.step) ();
+....disable std::mem::Throw(xec.step1504) ();
+....disable std::mem::Throw('%%args%%') ();
 ....disable std::mem::Limit(int) ();
+....disable std::mem::Limit(xec.step()) ();
 ....disable std::mem::Limit(xec.step1504() ();
+....disable std::mem::Limit(memory.cc) ();
 ....disable std::mem::Limit(%%$%int%.%int%%) ();
+....disable std::mem::Limit('%%args%%') ();
 ....disable std::mem::eject(osibasic.rom) ();
+....disable std::mem::eject(xec.step()) ();
+....disable std::mem::eject(xec.step1504()) ();
 ....disable std::mem::eject(%%DigitDecimalZeroDown%%) ();
-....disable std::mem::eject(%%ThrowDigitDecimalZeroDown%%) ();
+....disable std::mem::eject(%%ExpIntDigitDecimalZeroDown%%) ();
 ....disable std::mem::eject(%%$%1%.%0%%) ();
+....disable std::mem::eject(memory.cc) ();
+....disable std::mem::eject(rom.cc) ();
 ....disable std::mem::Eject(%%$%int%.%int%%) ();
+....disable std::mem::Eject('%%args%%') ();
 ....disable std::mem::disconnect::manifest(map) ();
 ....disable std::mem::disconnect::manifest(osibasic.rom) ();
 ....disable std::mem::disconnect::manifest(xec.step1504()) ();
+....disable std::mem::disconnect::manifest(xec.step()) ();
+....disable std::mem::disconnect::manifest(memory.cc) ();
+....disable std::mem::disconnect::manifest(rom.cc) ();
 ....disable std::mem::disconnect::manifest(%%DigitDecimalZeroDown%%) ();
-....disable std::mem::disconnect::manifest(%%ThrowDigitDecimalZeroDown%%) ();
+....disable std::mem::Disconnect::manifest(%%ExpIntDigitDecimalZeroDown%%) ();
 ....disable std::mem::disconnect::manifest(int) ();
 ....disable std::mem::disconnect::manifest(%%$%1%.%0%%) ();
 ....disable std::mem::Disconnect::manifest(%%$%int%.%int%%) ();
 ....disable std::mem::disconnect::manifest(%%$%Limiter%%) ();
 ....disable std::mem::disconnect::manifest(%%$%Limitter%%) ();
 ....disable std::mem::disconnect::manifest(%%$%DeLimiter%%) ();
-....disable std::mem::disconnect::manifest(%%$%DeLimitter%%) ();
+....disable std::mem::disconnect::manifest('%%args%%') ();
 ....disable std::mem::disconnect::context(map) ();
 ....disable std::mem::disconnect::context(osibasic.rom) ();
 ....disable std::mem::disconnect::context(xec.step1504()) ();
+....disable std::mem::disconnect::context(xec.step()) ();
 ....disable std::mem::disconnect::context(%%DigitDecimalZeroDown%%) ();
-....disable std::mem::disconnect::context(%%ThrowDigitDecimalZeroDown%%) ();
+....disable std::mem::disconnect::context(%%ExpIntDigitDecimalZeroDown%%) ();
 ....disable std::mem::disconnect::context(int) ();
-....disable std::mem::disconnect::context(%%int%.%int%%) ();) ();
+....disable std::mem::disconnect::context(memory.cc) ();
+....disable std::mem::disconnect::context(rom.cc) ();
 ....disable std::mem::disconnect::context(%%$%1%.%0%%) ();
+....disable std::mem::disconnect::context(%%DigitDecimalZeroDown%%) ();
+....disable std::mem::Disconnect::context(%%ExpIntDigitDecimalZeroDown%%) ();
 ....disable std::mem::disconnect::context(%%$%Limiter%%) ();
 ....disable std::mem::disconnect::context(%%$%Limitter%%) ();
 ....disable std::mem::disconnect::context(%%$%DeLimiter%%) ();
 ....disable std::mem::disconnect::context(%%$%DeLimitter%%) ();
+....disable std::mem::disconnect::context('%%args%%') ();
+
+Replay(w KeyValueWriter) 
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownError
+Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownDotPlacesThrowDigitDecimalZeroDown
+    Replay(w KeyValueWriter) ThrowExpIntDigitDecimalZeroDown
+	Replay(w KeyValueWriter) ThrowExpIntDigitZeroDown
+	Replay(w KeyValueWriter) ThrowExpIntThreeDigitZeroDown
+	Replay(w KeyValueWriter) ThrowExpIntEightDigitZeroDown
+	
+	...timeRefresh '%%1%s%%' () __replay;
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefresh '%%1%s%%' () __NewReplay;
+
+	..timeRefresh '%%1%s%%' () __run('%%args%%');
+	...call erase_if.h () _RepeatDuration (%%$%1%s%);
+	...timeRefresh '%%1%s%%' () __NewRun('%%args%%');
+
+}
 
 public void StartService()
         {
