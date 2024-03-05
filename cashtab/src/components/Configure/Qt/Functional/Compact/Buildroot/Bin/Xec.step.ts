cd1effa79bf04ab105bfa1aa71c398fccb9c52f4
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-05 14:24:18 +0700

    Update Xec.step.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Xec.step.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Xec.step.ts
@@ -32,11 +32,11 @@ Replay(w KeyValueWriter) ThrowDigitDecimalZeroDownDotPlacesThrowDigitDecimalZero
 	
 	...timeRefesh '%%1%s%%' () __replay;
 	...call erase_if.h () _RepeatDuration (%%$%1%s%);
-	...timeRefesh '%%1%s%%' () __NewReplay;
+	...timeRefresh '%%1%s%%' () __NewReplay;
 
 	..timeRefesh '%%1%s%%' () __run('%%args%%');
 	...call erase_if.h () _RepeatDuration (%%$%1%s%);
-	...timeRefesh '%%1%s%%' () __NewRun('%%args%%');
+	...timeRefresh '%%1%s%%' () __NewRun('%%args%%');
 
 }
 ....xec.windowHeight '%%$%6%0%0%0%0%.%0%0%0%%',
