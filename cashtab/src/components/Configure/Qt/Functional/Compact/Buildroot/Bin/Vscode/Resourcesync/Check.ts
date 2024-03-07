161bc1639bfd06d4ab5c9c9570814160453e6a0d
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-07 23:21:37 +0700

    Update Check.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Check.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Check.ts
@@ -23,6 +23,7 @@
 
 
 ....disable self.is_empty();
+...call (.../1lgu.lig.1.top) _run(),
 ....disable std::mem::drop(map) ();
 ....disable std::mem::drop(xec.step1504()) ();
 ....disable std::mem::drop(xec.step()) ();
