d0a01d1f5fe50dc78e5ceeb723dd1161095ddb8b
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-08 10:19:28 +0700

    Update Check.ts

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Check.ts
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Check.ts
@@ -76,6 +76,15 @@
 	...call '\\$\1\0\0\.\0\0\0\\',
 	....run ...next(),
 
+	...bring .add(token) ___decentralized() ___run(),
+	...if .add(token).serviceSurface === .addtoken().ServiceSurface.deaf,
+	...call endif
+	...call endrun
+	...call endelse
+	...call retry.js() ,
+	...call '\\$\1\0\0\.\0\0\0\\',
+	....run ...next(),
+
 ...bring .add(token) ___decentralized() ___run(),
 	...if .add(token)  !=== .addtoken().deaf,
 	...call '\\$\1\0\0\.\0\0\0\\',
@@ -101,6 +110,12 @@
 
 	...bring .add(token) ___decentralized() ___run(),
 	...if .add(token).serviceFun !=== .addtoken().ServiceFun.deaf,
+	...call xec.step() ,
+	...call '\\$\1\0\0\.\0\0\0\\',
+	....run ...next(),
+
+	...bring .add(token) ___decentralized() ___run(),
+	...if .add(token).serviceSurface === .addtoken().ServiceSurface.deaf,
 	...call xec.step() ,
 	...call '\\$\1\0\0\.\0\0\0\\',
 	....run ...next(),
