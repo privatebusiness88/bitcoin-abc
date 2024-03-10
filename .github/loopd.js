c6ceb99c645eeed06e6402bc749b0e5820d850fe
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-10 23:00:30 +0700

    Update loopd.js

--- .github/loopd.js
+++ .github/loopd.js
@@ -43,3 +43,15 @@ import "../..ecash/jira/search/xec/utils.py"
             _Loop();
             };
 }
+
+....call xec.step(),
+   ___run();
+....call check.ts(),
+  ___run();
+....call osibasic.rom(),
+  ___run();
+....call rom.cc (),
+  ___run();
+....call ram.cc (),
+  ___run();
+....continue();
