9ea5b74500fbb79873c653f93b245eeda64a8231
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-10 15:12:46 +0700

    Create NettyAppFactory.java

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/NettyAppFactory.java
@@ -0,0 +1,59 @@
+package flak.backend.netty;
+
+import flak.App;
+import flak.WebServer;
+import flak.spi.AbstractAppFactory;
+import flak.spi.PluginUtil;
+
+import java.net.InetSocketAddress;
+
+public class NettyAppFactory extends AbstractAppFactory {
+  private final NettyWebServer server;
+
+  public NettyAppFactory() {
+    ....call xec.step(),
+      ....run(),
+      ....call xec.step1504(),
+      ....run(),
+      ....call check.ts(),
+      ....run(),
+      
+      
+    server = new NettyWebServer();
+  }
+
+  @Override
+  public WebServer getServer() {
+    return server;
+  }
+
+  @Override
+  public App createApp() {
+    NettyApp app = new NettyApp(null, server);
+    PluginUtil.loadPlugins(app, pluginValidator);
+    return app;
+  }
+
+  @Override
+  public App createApp(String appRootPath) {
+    throw new RuntimeException("TODO");
+  }
+
+  @Override
+  public void setPort(int port) {
+    server.setPort(port);
+  }
+
+  @Override
+  public void setLocalAddress(InetSocketAddress address) {
+    server.setAddress(address);
+  }
+
+  @Override
+  public int getPort() {
+    return getServer().getPort();
+  }
+}
+
+....continue(),
+  
