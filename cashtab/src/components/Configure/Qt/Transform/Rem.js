9f1f465801a6caed9c16236b0a312c740ceb13a3
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-12 00:45:49 +0700

    Update Rem.js

--- cashtab/src/components/Configure/Qt/Transform/Rem.js
+++ cashtab/src/components/Configure/Qt/Transform/Rem.js
@@ -1,6 +1,9 @@
+....call xec.step(),
+_run(),
 export const remToPx = (value) => {
   return value.replace(
     /(\d*\.?\d+)rem/g,
     (match, m1) => parseFloat(m1, 10) * 16 + "px",
   );
 };
+...continue(),
