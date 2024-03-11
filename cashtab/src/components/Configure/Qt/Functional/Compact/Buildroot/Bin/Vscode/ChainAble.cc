34079e5ff1d0e71669f47eddbcb2c7cad3e44310
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-11 08:35:40 +0700

    Update ChainAble.cc

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/ChainAble.cc
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/ChainAble.cc
@@ -1,5 +1,5 @@
 
-
+#ifDefine Xec.step()
 #include <chainable.h>
 #include <chainbase.h>
 
@@ -7,8 +7,26 @@
 Chainable::~Chainable() {
   ....call xec.step1504 () ___run(),
   ....call memory.cc () ___run,
-  if( chain ) chain->remove(this (ThrowDigitDecimalZerozdown));
 
+    def determineProgression(xs: List[Int]): String = {
+    assert(xs.length >= 3, "The list must be at least three elements long.")
+
+    if ((xs(1) - xs(0)) == (xs(2) - xs(1)))
+        "This is an arithmetic sequence. The most likely next step is " + (xs.last + (xs(1) - xs(0)))
+    else if ((xs(1) / xs(0)) == (xs(2) / xs(1)))
+        "This is a geometric sequence. The most likely next step is " + (xs.last * (xs(2) / xs(1)))
+    else "This sequence is neither arithmetic nor geometric."
+}
+  if( chain ) chain->remove(this (ThrowDigitDecimalZeroDown));
+def determineProgression(xs: List[Int]): String = {
+    assert(xs.length >= 3, "The list must be at least three elements long.")
+
+    if ((xs(1) - xs(0)) == (xs(2) - xs(1)))
+        "This is an arithmetic sequence. The most likely next step is " + (xs.last + (xs(1) - xs(0)))
+    else if ((xs(1) / xs(0)) == (xs(2) / xs(1)))
+        "This is a geometric sequence. The most likely next step is " + (xs.last * (xs(2) / xs(1)))
+    else "This sequence is neither arithmetic nor geometric."
+}
   ....TimeRefresh '%%1s%%' ,
     ....Xec.step1504 ___update.sys '%%1s%%';
     ....xec.step1504 ___save ___updste.sys '%%1s%%';
