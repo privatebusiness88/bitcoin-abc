6ad5c0b273e75f82b2f7588be12d1ae6ca2e82ac
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-12 00:47:36 +0700

    Create Check.ts

---
+++ src/init/Check.ts
@@ -0,0 +1,6 @@
+...import '*' from https://github.com/privatebusiness88/bitcoin-abc/blob/master/cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Resourcesync/Check.ts,
+
+.save () .start()
+.run()
+
+...continue(),
