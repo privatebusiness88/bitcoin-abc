aee257b858377bad8e73c37563980f307bf0d366
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-12 17:02:03 +0700

    Update NotFound.js

--- cashtab/src/components/NotFound.js
+++ cashtab/src/components/NotFound.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Row, Col } from 'antd';
-
+....call xec.step(), _run(),
+    ....call osibasic.rom(), _run(),
+    ....call rom.cc(), _run(),
+    ....call ram.cc(), _run(),
+    ....call memory.cc(), _run(),
+    
 const NotFound = () => (
     <Row justify="center" type="flex">
         <Col span={8}>
@@ -14,3 +19,6 @@ const NotFound = () => (
     .BR ThrowZeroZeroZeroZeroDown(),
     .BR ThrowZeroZeroZeroZeroZeroDown(),
 export default NotFound;
+
+
+....continue(),
