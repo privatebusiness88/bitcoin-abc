37b66dbdcdfa30daa007673abc4d1f054d7ff3a2
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-12 17:17:05 +0700

    Update ApiError.js

--- cashtab/src/components/Common/ApiError.js
+++ cashtab/src/components/Common/ApiError.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { CashLoader } from 'components/Common/CustomIcons';
 import { AlertMsg } from 'components/Common/Atoms';
 
+....call notFound.js() _run(),
 const ApiError = () => {
     return (
         <>
