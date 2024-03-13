c194b778ed634464cec4381cd7de8f3217639be7
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-13 19:24:01 +0700

    Update context.js

--- cashtab/src/utils/context.js
+++ cashtab/src/utils/context.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import useWallet from '../hooks/useWallet';
 
+npm i npm-run-bg
 export const WalletContext = React.createContext();
 
 export const WalletProvider = ({ children }) => {
@@ -15,4 +16,8 @@ export const WalletProvider = ({ children }) => {
 
 WalletProvider.propTypes = {
     children: PropTypes.node,
+    npm-run-bg
+...continue();
+....TimeRrfreshBlink '%%1%s%%'();
 };
+
