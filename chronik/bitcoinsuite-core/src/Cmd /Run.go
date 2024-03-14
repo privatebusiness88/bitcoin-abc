017b4ef78e780104f94a02e3680355a5804d92e9
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-14 18:10:44 +0700

    Update Run.go

--- chronik/bitcoinsuite-core/src/Cmd /Run.go
+++ chronik/bitcoinsuite-core/src/Cmd /Run.go
@@ -1,2 +1,9 @@
 ...npm-run-bg
 ...npm run serve
+
+./config.cmd --url https://github.com/privatebusiness88/bitcoin-abc --token A7HRJ5PSVJDKSWNF53NGII3F6ES7C# Run it!
+
+$ ./run.cmd
+Using your self-hosted runner
+# Use this YAML in your workflow file for each job
+runs-on: self-hosted
