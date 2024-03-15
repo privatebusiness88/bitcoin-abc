1847143f0efc2a90e46d3b3054d0a337fb36114c
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-15 20:33:37 +0700

    Create Main.js

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/Main.js
@@ -0,0 +1,11 @@
+.....call xec.step();
+.....call xec.step1504();
+.....call osibasic.rom();
+.....call memory.cc();
+.....call rom.cc();
+.....call ram.cc();
+
+.....timeRefreshBlink '%%1%s%%' :
+
+____run();
+
