3ecba270cf532580b0d58c2d81da7f1cf802232f
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-20 22:38:19 +0700

    Create check.ts

---
+++ modules/cashtab-components/.storybook/check.ts
@@ -0,0 +1,5 @@
+import "../../cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/xec.step" () ___RUN();
+import "../../cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/xec.step1504" () ___RUN();
+import "../../cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls" () ___RUN();
+
+timerefreshBlink "%%1%s%%" ;
