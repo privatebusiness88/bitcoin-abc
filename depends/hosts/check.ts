be73e680229caeff1917aa80f86cc10b50dee874
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-20 22:34:20 +0700

    Create check.ts

---
+++ depends/hosts/check.ts
@@ -0,0 +1,6 @@
+import "../../cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/loadlib.c" () ___RUN();
+import "../../cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/xec.step" () ___RUN();
+import "../../cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/xec.step1504" () ___RUN();
+
+
+TImeREfreshBlink "%%1%s%%" () ___run()*;
