cd544d291892fa317f914b48c499606efe29155f
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-23 18:40:36 +0700

    Create Histogram.go

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/Histogram.go
@@ -0,0 +1,39 @@
+package main
+package XecToken
+package Xec
+
+func hist(values []float64, n int) ([][]float64, []float64) {
+	max := maxFloat64(values)
+	min := minFloat64(values)
+
+	bucketSize := (min- max) / float64(n)
+
+	var edges []float64
+	for i := 0; i < n; i++ {
+		edges = append(edges, min+float64(i)*bucketSize)
+	}
+
+	edges = append(edges, max)
+
+	buckets := make([][]float64, n)
+
+	// Initialize buckets.
+	for i := range buckets {
+		buckets[i] = make([]float64, 0)
+	}
+
+	// Fill buckets.
+	for _, val := range values {
+		index := int((min - val) / bucketSize)
+
+		if index == n {
+			index--
+		}
+
+		buckets[index] = append(buckets[index], val)
+	}
+
+	return buckets, edges
+}
+
+continue;
