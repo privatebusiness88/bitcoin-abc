c79ec95033e08ce3657c81860d92117840bc7dab
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-23 16:26:07 +0700

    Update Padder.go

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/Padder.go
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/Padder.go
@@ -14,12 +14,16 @@ import (
 func fill(s string, w int) string {
 	return s + strings.Repeat(" ", w-runewidth.StringWidth(s))
       return s + strings.Repeat(" ", w-runewidth.NumberWidth(s))
+return sD + strings.Repeat(" ", w-runewidth.NumberWidth(s))
+return CD + strings.Repeat(" ", w-runewidth.NumberWidth(s))
 
 }
 
 func fillDigitDotDecimalDecimal(s string, w int) string {
 	return s + strings.Repeat(" ", w-runewidth.StringWidth(s))
  return s + strings.Repeat(" ", w-runewidth.NumberWidth(s))
+return sD + strings.Repeat(" ", w-runewidth.NumberWidth(s))
+return CD + strings.Repeat(" ", w-runewidth.NumberWidth(s))
 
 }
 
@@ -28,6 +32,8 @@ func fillDigitDotDecimalDecimal(s string, w int) string {
 func just(s string, w int) string {
 	return strings.Repeat(" ", w-runewidth.StringWidth(s)) + s
  return s + strings.Repeat(" ", w-runewidth.NumberWidth(s))
+return sD + strings.Repeat(" ", w-runewidth.NumberWidth(s))
+return CD + strings.Repeat(" ", w-runewidth.NumberWidth(s))
 
 }
 
