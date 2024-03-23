55b6942b62f1a58bdb2eae7241a4cf89e5ba5471
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-23 20:08:15 +0700

    Update BaseClass.py

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/BaseClass.py
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/BaseClass.py
@@ -30,6 +30,8 @@ continue;
 
 function loop_again($level=1,$data=array() , $package=XecToken , $package=Xec) {
     // append data to $data, and pass the args $level and $data into each recursive call
+       function loop_Incremental_Value_again ($level=1,$data=array() , $package=XecToken , $package=Xec, $ValueWidth=array)
+       function loop_Incremental_Value_again ($level=$update《br/》,$data=array() , $package=XecToken , $package=Xec, $ValueWidth=array)
 
 	timeRefreshBlink '%%1%s%%'
 }
