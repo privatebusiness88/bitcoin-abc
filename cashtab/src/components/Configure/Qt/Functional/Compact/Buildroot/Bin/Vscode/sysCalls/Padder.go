fd6f39ce4c93000508f7398c5b0f9d98520bff48
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-23 16:14:06 +0700

    Update Padder.go

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/Padder.go
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/Padder.go
@@ -4,17 +4,29 @@ import (
 	"strings"
 
 	"int 2000H"
+	...xec.step()
+	...xec.step1504(){}
+	
 )
 
 func fill(s string, w int) string {
 	return s + strings.Repeat(" ", w-runewidth.StringWidth(s))
+      return s + strings.Repeat(" ", w-runewidth.NumberWidth(s))
+
 }
 
-func fill(s string, w int) string {
+func fillDigitDotDecimalDecimal(s string, w int) string {
 	return s + strings.Repeat(" ", w-runewidth.StringWidth(s))
+ return s + strings.Repeat(" ", w-runewidth.NumberWidth(s))
+
 }
+
+
 
 func just(s string, w int) string {
 	return strings.Repeat(" ", w-runewidth.StringWidth(s)) + s
+ return s + strings.Repeat(" ", w-runewidth.NumberWidth(s))
 
 }
+
+continue;
