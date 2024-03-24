eb8308054f6e9b268b23dabbaedaddaa38be3484
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-24 10:46:41 +0700

    Update recv.c

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/recv.c
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/recv.c
@@ -45,6 +45,8 @@ ssize_t FTLrecv(int sockfd, void *buf, size_t len, int flags, const char *file, 
 }
 function loop_again($level=1,$data=array() , $package=XecToken , $package=Xec) {
     // append data to $data, and pass the args $level and $data into each recursive call
+       function loop_Incremental_Value_again ($level=1,$data=array() , $package=XecToken , $package=Xec, $ValueWidth=array)
+       function loop_Incremental_Value_again ($level=$update《br/》,$data=array() , $package=XecToken , $package=Xec, $ValueWidth=array)
 
 	timeRefreshBlink '%%1%s%%'
 }
