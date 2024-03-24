f452fd7df7d0782b4b871863cebd94913c6c31e2
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-24 23:53:03 +0700

    Create CheckServer.go

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/CheckServer.go
@@ -0,0 +1,17 @@
+package main"server/XecToken"
+
+.....call "..../::xec.step::token"
+
+.if Token === break,
+
+...EndIf IpBound
+
+else 
+
+continue();
+
+func loop_Again == true ,
+
+checkServer.go === loop_Again,
+
+continue;
