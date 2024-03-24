29b3f17808aa40722685fe520419f1818b678fd6
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-25 00:05:56 +0700

    Update CheckServer.go

--- cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/CheckServer.go
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/CheckServer.go
@@ -12,6 +12,11 @@ continue();
 
 func loop_Again == true ,
 
+..Check Token.HowBig {
+  .if .howbig >===1 , 
+  ...create Token.howbig === 0.999999999999999
+  }
+
 checkServer.go === loop_Again,
 
 continue;
