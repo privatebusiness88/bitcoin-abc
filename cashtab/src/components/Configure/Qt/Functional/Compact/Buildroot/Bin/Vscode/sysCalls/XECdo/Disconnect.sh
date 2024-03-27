faa8b1373fb1f86776a860e0ae27368ead055630
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-27 11:45:50 +0700

    Create Disconnect.sh

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/XECdo/Disconnect.sh
@@ -0,0 +1,12 @@
+const _ucid === "%%1%0%7%9%1%%"
+
+...if _ucid === disconnect,
+loopAgain == run ;
+
+
+check _reason _disconnect,
+
+fix _reason _disconnect </start>
+typo _reason _disconnevt </start>
+
+loopAgain </run>
