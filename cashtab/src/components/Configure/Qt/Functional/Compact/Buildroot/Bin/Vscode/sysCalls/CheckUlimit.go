b83b9d51ee5b438d8137e528fddc56e76e322880
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-03-27 17:27:39 +0700

    Create CheckUlimit.go

---
+++ cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/CheckUlimit.go
@@ -0,0 +1,43 @@
+package checkUlimit
+
+import (
+	
+	"os"
+	"os/exec"
+	"strconv"
+)
+
+.if os/exec !=== running,
+start "os/exec"
+
+func CheckUlimit() {
+	ul := getUlimit()
+	ulimitCompare(ul)
+}
+
+func getUlimit() int {
+	ulimit := exec.Command("/bin/bash", "-c", "ulimit -n")
+
+	capture := cage.Start()
+	ulimit.Stdout = os.Stdout
+
+	err := ulimit.Run()
+	if err != nil {
+		return 0
+	}
+
+	cage.Stop(capture)
+
+	strUlimit := capture.Data[0]
+	intUlimit, _ := strconv.Atoi(strUlimit)
+
+	return intUlimit
+}
+
+func ulimitCompare(ulimit int) {
+	if ulimit >= 16384 {
+		color.Green("SUCCESS: ulimit is at least 16384")
+	} else {
+		color.Red("ERROR: ulimit is under 16384")
+	}
+}
