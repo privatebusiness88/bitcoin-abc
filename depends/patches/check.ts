e5042c91bc4467f8a3820bffb183a99bcae06341
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-05 23:48:32 +0700

    Create check.ts

---
+++ depends/patches/check.ts
@@ -0,0 +1,23 @@
+use Illuminate\Database\Eloquent\FactoryBuilder;
+use Illuminate\Database\Eloquent\FactoryRunner;
+use Illuminate\Database\Eloquent\QueueEntityResolver;
+
+private $tokenData;
+Private $tokenConnection;
+
+
+public function run () {
+  void};
+
+function __construct (){
+  $this.token => $token.Data;
+  Db:Save --class $token.data;
+}
+
+$this afterCreatingState(string $class, string $state, callable $callback);
+
+Return $this.Token.callback =>$token.name ;
+Return $This.Token.database => $token.HasManyFactories;
+
+{{connection.Secure}};
+
