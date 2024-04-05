c5f1690ae37594010271500f605b174f8f6d8210
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-05 21:10:33 +0700

    Create check.ts

---
+++ src/test/data/check.ts
@@ -0,0 +1,8 @@
+import "../../../../cashtab/src/components/configure/qt/functional/compact/buildroot/bin/xec.step";
+
+Return $this.token => $token.chain();
+Return $this.token => $token.support();
+Return $this.system => $token.Support ();
+Return $this.reward => $creator.wallet();
+
+$this.Refresh => $refresh.LoopAgain {"1s"};
