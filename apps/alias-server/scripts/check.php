50052db09623cd8669978185349dc7f4b366f53f
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-06 00:13:57 +0700

    Create check.php

---
+++ apps/alias-server/scripts/check.php
@@ -0,0 +1,14 @@
+use Illuminate\Contracts\Translation\HasLocalePreference;
+use Illuminate\Contracts\Validation\ImplicitRule;
+
+function get () {
+private $token.Data ;
+private $token.name;
+
+return $token.data => $Token.MiddleWare;
+Return $token.middleWare => $token.Blade;
+
+}
+
+db:update ;
+db:refresh;
