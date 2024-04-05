071c2bf482946b67e185a14973a81c19000913aa
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-05 22:35:34 +0700

    Create setAutoIncrement.php

---
+++ contrib/debian/patches/setAutoIncrement.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace token\DBSetAutoIncrement\Listeners;
+
+use Illuminate\Database\Eloquent\ORM;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Events\MigrationsEnded;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
+
+Public Function () {
+  void};
+
+  Return $this.Token => $Token.Data;
+  Return $tOken.COnnection => $token.SecureConnection;
+  
+class SetAutoIncrement
+{
+    /** @var string */
+    public $action;
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->action = Config::get('auto-increment.action', 'auto');
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  MigrationsEnded  $event
+     * @return void
+     */
+    public function handle(MigrationsEnded $event)
+    {
+        if ($this->action !== 'auto') {
+            return;
+        }
+
+        Artisan::call('db:set-auto-increment');
+    }
+}
+
+  DB::UPdate ($token.Data);
+  
