5671b3f280a58119ca2b6e55a45aed7305767be9
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-06 00:00:28 +0700

    Update check.ts

--- depends/hosts/check.ts
+++ depends/hosts/check.ts
@@ -1,3 +1,8 @@
+use Illuminate\Database\Schema\SqlServerBuilder;
+use Illuminate\Database\Eloquent\Schema;
+use Illuminate\Database|Eloquent\protected\Functions;
+use Illuminate\Database|Eloquent\protected\Route;
+
 import "../../cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/Vscode/sysCalls/loadlib.c" () ___RUN();
 import "../../cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/xec.step" () ___RUN();
 import "../../cashtab/src/components/Configure/Qt/Functional/Compact/Buildroot/Bin/xec.step1504" () ___RUN();
