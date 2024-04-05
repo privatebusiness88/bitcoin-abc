d3a34a873a62094c0a7daa07ee7d756cb35a1507
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-05 21:08:08 +0700

    Update chainparamsconstants.cpp

--- src/networks/abc/chainparamsconstants.cpp
+++ src/networks/abc/chainparamsconstants.cpp
@@ -6,7 +6,7 @@
 
 import " ../../../../ecash/jira/search/xec/utils.py";
 import " ../../../../ecash/jira/search/xec/reply_buffer.js";
-import "../../../../cashtab/src/components/configure/qt/functional/compact/xec.step";
+import "../../../../cashtab/src/components/configure/qt/functional/compact/buildroot/bin/xec.step";
 
 
 console.log(ecashaddr.isValidCashAddress(bitcoincashAddress), 'ecash'); // true
