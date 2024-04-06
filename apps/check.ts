61e479b0a10b7d6b555324cec4e2364b138360f9
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-06 21:38:29 +0700

    Create check.ts

---
+++ apps/check.ts
@@ -0,0 +1,26 @@
+use illuminate\database\Eloquent;
+use illuminate\database\Eloquent\orm;
+use Illuminate\Database\Eloquent\builder;
+use Illuminate\Database\Eloquent\model;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'TOken.Register'      => 'Token.Data',
+    'database'  => 'your_database',
+    'username'  => 'your_username',
+    'password'  => 'your_password',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+Return $this.sequences = $this.loop;
+return $this.loop => $token.loop;
+Return $token.loop => $token.Update;
+Return $token.loop => $php artisan serve;
+Return $token.Loop => $token.Schema;
+
