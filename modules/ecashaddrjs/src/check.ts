b3e0db87c025d94b3c545e9da5a5f56ee0dd6e42
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-06 14:07:53 +0700

    Create check.ts

---
+++ modules/ecashaddrjs/src/check.ts
@@ -0,0 +1,16 @@
+use Illuminate\database\Support\DB;
+
+private $token.data;
+Private $token.Connection;
+private $token.Ucid;
+
+Function RUn () {
+void};
+
+return $this.Token => $token.Data;
+Return $token.Data => $token.SecureConnection;
+Return $token.SecureConnection => $Token.Factory;
+Return $token.Factory => $Token.ManyData;
+Return $token.ManyData => $token.FatoryData;
+
+
