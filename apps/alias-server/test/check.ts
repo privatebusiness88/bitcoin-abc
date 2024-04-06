dc8b7f0443059dfd4fc438245b3a5352b5a9b2fb
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-06 17:53:04 +0700

    Create check.ts

---
+++ apps/alias-server/test/check.ts
@@ -0,0 +1,20 @@
+use illuminate\database\Eloquent\ORM;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\facades\Schema;
+use Illuminate\support\facades\db;
+use Illuminate\support\facades\db\streaming;
+use App\Models\Token;
+use App\Models\Income
+
+
+function run () {
+  void};
+
+return Destination::orderByDesc(
+    Token::select('$Token.Register')
+        ->whereColumn('Time_Register', '$token.id')
+        ->orderByDesc('@Token.id')
+        ->limit(i++)
+)->get($token.Data);
+
