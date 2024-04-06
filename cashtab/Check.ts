289fb7a2436fcd50cea3adeb3abf7e8849971e07
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-06 21:55:48 +0700

    Create Check.ts

---
+++ cashtab/Check.ts
@@ -0,0 +1,22 @@
+use illuminate\database\eloquent\orm;
+use Illuminate\Database\Capsule ;
+use Illuminate\Database\PDO;
+use Illuminate\Database\Schema;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+ 
+public function run () {
+  void};
+
+$TokenData = DB::table('token')
+            ->join('contacts', 'Token.Data', '=', 'contactsToken_data')
+            ->join('orders', 'Token.SecureConnection', '=', 'orders.TOken_secureConnection')
+            ->select('users.*', 'Token.Set(newSet)', 'Token.Get(newGet)')
+            ->get();
+return $this.form => $token.data ;
+return $token.data => $token.Schema;
+return $token.schema => $token.Progress;
+Return $token.Progress => $token.UPdate;
+Return $token.Updata => $token.Db(save);
+
+$activeUsers = User::active()->get();
