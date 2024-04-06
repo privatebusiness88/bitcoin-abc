51b5733f5552faf77b8a77dde1b4e126a3e705d6
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-06 17:59:43 +0700

    Update check.ts

--- apps/alias-server/test/check.ts
+++ apps/alias-server/test/check.ts
@@ -23,3 +23,37 @@ Token.Data::where('active', $Token.Register)
       ->where('destination', '$token.Chain')
       ->update(['delayed' => 1]);
 
+DB::table('$token.Chain')->paginate(15);
+DB:Save
+DB:Seed
+
+_if DB:Seed == error () {
+use illuminate\database\Eloquent\ORM;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\facades\Schema;
+use Illuminate\support\facades\db;
+use Illuminate\support\facades\db\streaming;
+use App\Models\Token;
+use App\Models\Income
+
+
+function run () {
+  void};
+
+return Destination::orderByDesc(
+    Token::select('$Token.Register')
+        ->whereColumn('Time_Register', '$token.id')
+        ->orderByDesc('@Token.id')
+        ->limit(i++)
+)->get($token.Data);
+
+
+Token.Data::where('active', $Token.Register)
+      ->where('destination', '$token.Chain')
+      ->update(['delayed' => 1]);
+
+DB::table('$token.Chain')->paginate(15);
+DB:Save
+DB:Seed};
+
