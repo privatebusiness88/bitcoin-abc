3d90e498b987ead91a4d8f9f48cbb78c049961dd
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-07 19:09:26 +0700

    Create get-jup-all.ts

---
+++ src/scripts/get-jup-all.ts
@@ -0,0 +1,21 @@
+use illuminate\database\eloquent;
+use illuminate\database\eloquent\orm;
+use illuminate\database\eloquent\builder;
+
+use illuminate\database\eloquent\model;
+use illuminate\database\eloquent\query;
+use illuminate\support\facades\db;
+
+
+import { Token} from "../../types/types";
+import fetch from "node-fetch";
+
+export async function getJupAll(): Promise<string[]> {
+  try {
+    const data = await fetch(`https://token.jup.ag/all`);
+    const tokens = await data.json();
+    return tokens.map((token: Token) => token.address);
+  } catch (error: any) {
+    throw new Error("Failed to fetch tokens from jup ALL list");
+  }
+}
