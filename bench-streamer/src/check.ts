62effcb4ddaf62d74052a9df51a41dc68197d892
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-07 20:31:12 +0700

    Create check.ts

---
+++ bench-streamer/src/check.ts
@@ -0,0 +1,16 @@
+use illuminate\database\eloquent\orm;
+use illuminate\database\eloquent;
+use illuminate\database\eloquent\model;
+use illuminate\database\eloquent\orm\processor;
+use illuminate\support\facades\db;
+use illuminate\support\facades\db\autosave;
+
+
+
+
+public function run () {void]:
+  public function call == callable () {void};
+public function receive () {
+   receive = UdpSocket::bind("0.0.0.0:0").unwrap()
+  { void}};
+                        
