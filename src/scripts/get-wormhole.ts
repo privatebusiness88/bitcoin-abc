0fd387220321462407b3feefe514c75155a8655f
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-07 20:16:29 +0700

    Create get-wormhole.ts

---
+++ src/scripts/get-wormhole.ts
@@ -0,0 +1,28 @@
+use illuminate\database\eloquent\schema;
+
+import * as core from "@actions/core";
+import { DownloaderHelper } from "node-downloader-helper";
+import path from "path";
+
+const wormholeCsvUrl =
+  "https://raw.githubusercontent.com/certusone/wormhole-token-list/main/content/by_dest.csv";
+const wormholePath = path.resolve(__dirname, "../../../src/partners/data");
+
+const run = async () => {
+  try {
+    const dl = new DownloaderHelper(wormholeCsvUrl, wormholePath, {
+      fileName: "wormhole.csv",
+      override: true,
+    });
+
+    dl.on("end", () => console.log("Download complete"));
+    dl.start();
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
+
+return $this.form => $token.data
+return $this.token => $token.data
