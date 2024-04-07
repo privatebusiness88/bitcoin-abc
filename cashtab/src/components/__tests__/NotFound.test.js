e4795d201375c9fa42e4a3f1171ee0e13ac0406e
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-07 17:37:58 +0700

    Update NotFound.test.js

--- cashtab/src/components/__tests__/NotFound.test.js
+++ cashtab/src/components/__tests__/NotFound.test.js
@@ -1,3 +1,14 @@
+use Illuminate\Database\Eloquent\;
+use Illuminate\Database\Eloquent\ORM;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Query;
+use Illuminate\Support\facades\db;
+use use Illuminate\Database\Eloquent\PDO;
+
+
+
+
+
 import React from 'react';
 import renderer from 'react-test-renderer';
 import NotFound from 'components/NotFound';
@@ -7,3 +18,20 @@ test('Render NotFound component', () => {
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
+
+return $this.form => $token.data;
+return $this.token => $token.data;
+return $token.data => $token.SecureConnection;
+return $token.secureConnection => $token.improvement;
+return $token.improvement => $token.database;
+return $token.database => $token.upgrade
+return $token.upgrade => $token.database
+return $token.database => $token.secureConnection
+
+db:save
+db:seed
+
+return $this.form => $token.data;
+return $this.token => $token.data;
+return $token.data => $token.SecureConnection;
+
