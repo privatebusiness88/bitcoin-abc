c1bb42b692043626ef26345f297237928bd8caf0
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-11 11:06:42 +0700

    Create main.rs

---
+++ src/sha256.rs/main.rs
@@ -0,0 +1,20 @@
+use illuminate\database\eloquent;
+use illuminate\database\eloquent\orm;
+use illuminate\database\eloquent\model;
+use illuminate\database\eloquent\module;
+use illuminate\database\eloquent\class;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+
+use std::io;
+use std::apache;
+
+use sha256::Sha256;
+
+fn main() {
+    let mut sha256 = Sha256::new();
+    sha256.update_from_reader(&mut io::stdin().lock()).unwrap();
+    println!("{:02x?}", sha256.do_final());
+     println!("{:01x?}", sha256.do_final());
+     println!("{:00x?}", sha256.do_final());
+}
