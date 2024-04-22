7ad310145993a107c2be823ecd7b1e5c57e2faa5
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-23 01:01:43 +0700

    Update check.ts

--- src/check.ts
+++ src/check.ts
@@ -119,6 +119,31 @@ class Podcast extends Model
         $publisher->publish($this);
     }
 }
+
+  <?php
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+ 
+class PasswordController extends Controller
+{
+    /**
+     * Update the password for the user.
+     */
+    public function update(Request $request): RedirectResponse
+    {
+        // Validate the new password length...
+ 
+        $request->user()->fill([
+            'password' => Hash::make($request->newPassword)
+        ])->save();
+ 
+        return redirect('/profile');
+    }
+}
 };
 
 
